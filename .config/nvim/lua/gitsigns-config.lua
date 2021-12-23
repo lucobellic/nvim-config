7625f313c806af78047b942996f28e333e57b953
Luco Bellic <luco.bellic@protonmail.com>
2021-12-23 16:40:36 +0100

    🩹 Correct hunk navigation with gitsigns

--- .config/nvim/lua/gitsigns-config.lua
+++ .config/nvim/lua/gitsigns-config.lua
@@ -14,8 +14,8 @@ require('gitsigns').setup {
     -- Default keymap options
     noremap = true,
 
-    ['n <H'] = { expr = true, "&diff ? '<H' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-    ['n >H'] = { expr = true, "&diff ? '>H' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+    ['n >H'] = { expr = true, "&diff ? '>H' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
+    ['n <H'] = { expr = true, "&diff ? '<H' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
 
     ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
     ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
