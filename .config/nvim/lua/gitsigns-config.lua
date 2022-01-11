9a3579ad106fc69cc2e07ed03774795b05152fd8
Luco Bellic <luco.bellic@protonmail.com>
2022-01-11 01:09:53 +0100

    👽 Update configuration due to gitsigns API changes

--- .config/nvim/lua/gitsigns-config.lua
+++ .config/nvim/lua/gitsigns-config.lua
@@ -17,20 +17,21 @@ require('gitsigns').setup {
     ['n >H'] = { expr = true, "&diff ? '>H' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
     ['n <H'] = { expr = true, "&diff ? '<H' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
 
-    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <leader>hv'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-    ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
+    ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
+    ['v <leader>hs'] = ':Gitsigns stage_hunk<CR>',
+    ['n <leader>hu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
+    ['n <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
+    ['v <leader>hr'] = ':Gitsigns reset_hunk<CR>',
+    ['n <leader>hR'] = '<cmd>Gitsigns reset_buffer<CR>',
+    ['n <leader>hv'] = '<cmd>Gitsigns preview_hunk<CR>',
+    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
+    ['n <leader>hS'] = '<cmd>Gitsigns stage_buffer<CR>',
+    ['n <leader>hU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
+    ['n <leader>ht'] = '<cmd>Gitsigns toggle_current_line_blame<CR>',
 
     -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+    ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
+    ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
   },
   watch_gitdir = {
     interval = 1000,
