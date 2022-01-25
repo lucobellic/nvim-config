c1158940f3bb917e15a62b065a4e6c4d7b607321
Luco Bellic <luco.bellic@protonmail.com>
2022-01-25 19:00:08 +0100

    🔧 Update lsp keymap and diagnostic error

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,5 +1,4 @@
 local nvim_lsp = require('lspconfig')
--- local saga = require('lspsaga')
 
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -8,37 +7,25 @@ local on_attach = function(client, bufnr)
   -- Mappings
   local opts = { noremap=true, silent=true }
 
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  -- buf_set_keymap('n', 'K', '<cmd>lua require(\'lspsaga.hover\').render_hover_doc()<CR>', opts)
---  buf_set_keymap('n', '<C-j>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(1)<CR>', opts)
---  buf_set_keymap('n', '<C-k>', '<cmd>lua require(\'lspsaga.action\').smart_scroll_with_saga(-1)<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- buf_set_keymap('n', '<C-k>', '<cmd>lua require(\'lspsaga.signaturehelp\').signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-
--- map <silent> <M-o> :CocCommand clangd.switchSourceHeader<CR>
--- buf_set_keymap('n', '<M-0>', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
---  buf_set_keymap('n', '<space>D', '<cmd>lua require(\'lspsaga.provider\').preview_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<F2>',  '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  -- buf_set_keymap('n', '<space>rn', '<cmd>lua require(\'lspsaga.rename\').rename()<CR>', opts)
-  -- buf_set_keymap('n', '<F2>', '<cmd>lua require(\'lspsaga.rename\').rename()<CR>', opts)
-
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- buf_set_keymap('n', '<space>ca', '<cmd>lua require(\'lspsaga.codeaction\').code_action()<CR>', opts)
-  -- buf_set_keymap('v', '<space>ca', '<cmd>lua require(\'lspsaga.codeaction\').range_code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  -- buf_set_keymap('n', 'gr', '<cmd>lua require(\'lspsaga.provider\').lsp_finder()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '<C', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', '>C', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap('n', 'gD'        , '<Cmd>lua vim.lsp.buf.declaration()<CR>'                               , opts)
+  buf_set_keymap('n', 'gd'        , '<Cmd>lua vim.lsp.buf.definition()<CR>'                                , opts)
+  buf_set_keymap('n', 'K'         , '<Cmd>lua vim.lsp.buf.hover()<CR>'                                     , opts)
+  buf_set_keymap('n', '<leader>d' , '<Cmd>lua vim.lsp.buf.hover()<CR>'                                     , opts)
+  buf_set_keymap('n', 'gi'        , '<cmd>lua vim.lsp.buf.implementation()<CR>'                            , opts)
+  buf_set_keymap('n', '<C-k>'     , '<cmd>lua vim.lsp.buf.signature_help()<CR>'                            , opts)
+  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>'                      , opts)
+  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>'                   , opts)
+  buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<leader>D' , '<cmd>lua vim.lsp.buf.type_definition()<CR>'                           , opts)
+  buf_set_keymap('n', '<space>rn' , '<cmd>lua vim.lsp.buf.rename()<CR>'                                    , opts)
+  buf_set_keymap('n', '<F2>'      , '<cmd>lua vim.lsp.buf.rename()<CR>'                                    , opts)
+  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>'                               , opts)
+  buf_set_keymap('n', '<leader>cf', '<cmd>lua vim.lsp.buf.code_action({nil, "quickfix"})<CR>'              , opts)
+  buf_set_keymap('n', 'gr'        , '<cmd>lua vim.lsp.buf.references()<CR>'                                , opts)
+  buf_set_keymap('n', '<leader>e' , '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>'              , opts)
+  buf_set_keymap('n', '<C'        , '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>'                          , opts)
+  buf_set_keymap('n', '>C'        , '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>'                          , opts)
+  buf_set_keymap('n', '<leader>q' , '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>'                        , opts)
 
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
@@ -48,21 +35,66 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 end
+
+-- Add additional capabilities supported by nvim-cmp
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "rust_analyzer" }
+local servers = { 'pyright', 'rust_analyzer' }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach
+    -- capabilities = capabilities
+  }
 end
 
 -- C/C++ Clangd configuration
+-- local nvim_lsp_clangd_highlight = require('nvim-lsp-clangd-highlight')
 nvim_lsp.clangd.setup {
+  -- on_init = nvim_lsp_clangd_highlight.on_init,
   on_attach = on_attach,
-  cmd = {"clangd", "--background-index", "--clang-tidy", "--enable-config" },
+  cmd = { "clangd", "--background-index", "--clang-tidy", "--enable-config" },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
   settings = {
-     semanticHighlighting = true
-  }
+    semanticHighlighting = true
+  },
+  -- capabilities = capabilities
+  capabilities = {
+    textDocument = {
+      semanticHighlightingCapabilities = {
+        semanticHighlighting = true
+      }
+    }
+  },
 }
+
+local signs = { Error = "•", Warn = "•", Hint = "•", Info = "•" }
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true
+})
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Highlight line number instead of having icons in sign column
+vim.cmd [[
+  hi link DiagnosticLineNrError ErrorMsg
+  hi link DiagnosticLineNrWarn WarningMsg
+  hi link DiagnosticLineNrInfo DiffChange
+  hi link DiagnosticLineNrHint DiffAdd
+
+  sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
+  sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
+  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
+  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
+]]
 
 -- vim.cmd('hi link DiagnosticError TODO')
 -- vim.cmd('hi link DiagnosticWarning TODO')
@@ -72,6 +104,7 @@ nvim_lsp.clangd.setup {
 
 -- cmd('nnoremap <silent> g; <cmd>lua require(\'lspsaga.floaterm\').open_float_terminal(\'lazygit\')<CR>')
 -- cmd('tnoremap <silent> g; <C-\\><C-n>:lua require(\'lspsaga.floaterm\').close_float_terminal()<CR>')
+
 
 
 
