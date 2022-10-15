dc71a2990181c7d745c48567d080d11cb3e3482b
Luco Bellic <luco.bellic@protonmail.com>
2022-10-15 18:11:43 +0200

    ✨ Set luasnip tab completion

--- lua/plugin/completion/comp.lua
+++ lua/plugin/completion/comp.lua
@@ -1,14 +1,20 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 vim.cmd([[set completeopt=menu,menuone,noselect]])
 
 -- Setup nvim-cmp.
 local lspkind = require('lspkind')
+local luasnip = require('luasnip')
 local cmp = require'cmp'
 
 cmp.setup({
   snippet = {
     expand = function(args)
       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
@@ -27,7 +33,44 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    -- Intellij-like mapping
+    -- ["<tab>"] = cmp.mapping(function(fallback)
+    --   -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+    --   if cmp.visible() then
+    --     local entry = cmp.get_selected_entry()
+    --     if not entry then
+    --       cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    --     else
+    --       cmp.confirm()
+    --     end
+    --   else
+    --     fallback()
+    --   end
+    -- end, {"i","s","c",}),
   }),
   sources = {
     { name = 'nvim_lsp' },
@@ -69,6 +112,4 @@ cmp.setup.cmdline(':', {
   })
 })
 
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
