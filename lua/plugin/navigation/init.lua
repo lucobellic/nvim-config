a154f5eddd6876e1d534eb192d19c42ac73052ec
Luco Bellic <luco.bellic@protonmail.com>
2022-10-15 23:26:35 +0200

    ➕ Add vim-repeat dependency

--- lua/plugin/navigation/init.lua
+++ lua/plugin/navigation/init.lua
@@ -8,6 +8,7 @@ return {config = function(use)
     use  'tpope/vim-commentary'
     use  'tpope/vim-fugitive'
     use  'tpope/vim-sleuth'
+    use  'tpope/vim-repeat'
 
 
     use {'kyazdani42/nvim-tree.lua', config = function() require('plugin.navigation.tree') end}
