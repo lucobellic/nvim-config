eaa379ff61c2b30671b091f77cac28f35f448a2c
Luco Bellic <luco.bellic@protonmail.com>
2022-10-18 00:20:30 +0200

    🔧 Disable treesitter on server of semantic tokens

--- lua/plugin/ui/treesitter.lua
+++ lua/plugin/ui/treesitter.lua
@@ -1,15 +1,10 @@
 local cmd = vim.cmd
 
-cmd('hi link TSParameter Constant')
-cmd('hi link TSParameterReference Constant')
-cmd('hi link TSNamespace String')
-cmd('hi link TSField Todo')
-
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"norg", "python"},
+  -- ensure_installed = {"norg", "python"},
   highlight = {
     enable = true,
-    disable = {"c", "cpp", "rust"},
+    disable = {"c", "cpp", "rust", "python", "lua"}, -- disable highlight supported by lsp
     custom_captures = {
       -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
       -- ["foo.bar"] = "Constant",
