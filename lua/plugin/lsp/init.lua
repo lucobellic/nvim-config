330cd507335742bab13cdfd587d188d6208a4397
Luco Bellic <luco.bellic@protonmail.com>
2022-10-18 00:17:25 +0200

    ➕ Add lsp semantic tokens

--- lua/plugin/lsp/init.lua
+++ lua/plugin/lsp/init.lua
@@ -1,25 +1,10 @@
 return { config = function(use)
-    use {'neoclide/coc.nvim',
-      branch = 'release',
-      opt = false,
-      -- Keep proper cpp highliglith from coc-highlight and coc-clang as lsp
-      run = ':CocInstall coc-highlight coc-clang',
-      config = function()
-        if vim.g.lsp_provider == 'coc' then
-          vim.cmd('source ' .. config_path .. '/' .. 'coc.vim')
-        else
-          vim.fn['coc#config']('diagnostic', { enable = false })
-        end
-        -- vim.cmd('source ' .. config_path .. '/' .. 'coc-explorer.vim')
-      end
-    }
-
-
     use {'williamboman/mason.nvim', config = function() require('mason').setup() end }
     use {'williamboman/mason-lspconfig.nvim', config = function() require('mason-lspconfig').setup() end }
 
     -- Use native nvim lsp
-    use {'neovim/nvim-lspconfig', config = function() require('plugin.lsp.config') end}
+    use {'theHamsta/nvim-semantic-tokens', config = function() require('plugin.lsp.tokens') end}
+    use {'neovim/nvim-lspconfig', after = 'nvim-semantic-tokens', config = function() require('plugin.lsp.config') end}
 
     use {'glepnir/lspsaga.nvim', config = function() require('plugin.lsp.saga') end}
 
