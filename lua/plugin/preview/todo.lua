47336017e3a789bb32b158f2fdac0384e66e102d
Luco Bellic <luco.bellic@protonmail.com>
2022-10-19 00:17:11 +0200

    💄 Update todotrouble rendering

--- lua/plugin/preview/todo.lua
+++ lua/plugin/preview/todo.lua
@@ -29,7 +29,7 @@ require('todo-comments').setup{
     highlight = {
         before = "", -- "fg" or "bg" or empty
         keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-        after = "", -- "fg" or "bg" or empty
+        after = "fg", -- "fg" or "bg" or empty
         pattern = [[.*<(KEYWORDS)(\(.*\))?\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
         comments_only = true, -- uses treesitter to match keywords in comments only
         max_line_len = 400, -- ignore lines longer than this
