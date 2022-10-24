dfa373d7830eb25a6373e4e66cc0fbec4abd18e0
Luco Bellic <luco.bellic@protonmail.com>
2022-10-24 22:10:51 +0200

    ✨ Enable treesitter for python

--- lua/plugin/ui/treesitter.lua
+++ lua/plugin/ui/treesitter.lua
@@ -4,7 +4,7 @@ require'nvim-treesitter.configs'.setup {
   -- ensure_installed = {"norg", "python"},
   highlight = {
     enable = true,
-    disable = {"c", "cpp", "rust", "python", "lua"}, -- disable highlight supported by lsp
+    disable = {"c", "cpp", "rust", "lua"}, -- disable highlight supported by lsp
     custom_captures = {
       -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
       -- ["foo.bar"] = "Constant",
