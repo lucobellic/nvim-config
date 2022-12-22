70ac17c84c99945e6c62d200cbcc92639dd7ca44
Luco Bellic <luco.bellic@protonmail.com>
2022-12-22 15:40:02 +0100

    🚚 Move hop mapping in dedicated file

--- plugin/z_mappings.lua
+++ plugin/z_mappings.lua
@@ -135,3 +135,21 @@ if packer_plugins and packer_plugins['vim-floaterm'] then
         end
     })
 end
+
+-- Hop
+if packer_plugins and packer_plugins['hop.nvim'] then
+    vim.api.nvim_set_keymap('n', '<leader>j', "<cmd>lua require'hop'.hint_words()<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>j', "<cmd>lua require'hop'.hint_words()<cr>", {})
+    vim.api.nvim_set_keymap('n', '<leader>J', "<cmd>lua require'hop'.hint_words({multi_windows = true})<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>J', "<cmd>lua require'hop'.hint_words({multi_windows = true})<cr>", {})
+
+    vim.api.nvim_set_keymap('n', '<leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>l', "<cmd>lua require'hop'.hint_lines()<cr>", {})
+    vim.api.nvim_set_keymap('n', '<leader>L', "<cmd>lua require'hop'.hint_lines({multi_windows = true})<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>L', "<cmd>lua require'hop'.hint_lines({multi_windows = true})<cr>", {})
+
+    vim.api.nvim_set_keymap('n', '<leader>s', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>s', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+    vim.api.nvim_set_keymap('n', '<leader>S', "<cmd>lua require'hop'.hint_char1({multi_windows = true})<cr>", {})
+    vim.api.nvim_set_keymap('v', '<leader>S', "<cmd>lua require'hop'.hint_char1({multi_windows = true})<cr>", {})
+end
