8e9a319d7a292d164c66711c0e3d6e6b4d42b741
Luco Bellic <luco.bellic@protonmail.com>
2022-12-30 11:33:05 +0100

    💄 [lspsaga] Update outline visualisation

--- lua/plugin/lsp/saga.lua
+++ lua/plugin/lsp/saga.lua
@@ -79,10 +79,10 @@ saga.init_lsp_saga({
         -- defx, db_ui
         win_with = '',
         win_width = 50,
-        auto_enter = false,
+        auto_enter = true,
         auto_preview = false,
         virt_text = '┃',
-        jump_key = 'o',
+        jump_key = '<Enter>',
         -- auto refresh when change buffer
         auto_refresh = true,
     },
@@ -112,52 +112,52 @@ saga.init_lsp_saga({
 })
 
 -- Symbols in winbar
-local function get_file_name(include_path)
-    local file_name = require('lspsaga.symbolwinbar').get_file_name()
-    if vim.fn.bufname '%' == '' then return '' end
-    if include_path == false then return file_name end
-    -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
-    local sep = vim.loop.os_uname().sysname == 'Windows' and '\\' or '/'
-    local path_list = vim.split(string.gsub(vim.fn.expand '%:~:.:h', '%%', ''), sep)
-    local file_path = ''
-    for _, cur in ipairs(path_list) do
-        file_path = (cur == '.' or cur == '~') and '' or
-            file_path .. cur .. ' ' .. '%#LspSagaWinbarSep#>%*' .. ' %*'
-    end
-    return file_path .. file_name
-end
+-- local function get_file_name(include_path)
+--     local file_name = require('lspsaga.symbolwinbar').get_file_name()
+--     if vim.fn.bufname '%' == '' then return '' end
+--     if include_path == false then return file_name end
+--     -- Else if include path: ./lsp/saga.lua -> lsp > saga.lua
+--     local sep = vim.loop.os_uname().sysname == 'Windows' and '\\' or '/'
+--     local path_list = vim.split(string.gsub(vim.fn.expand '%:~:.:h', '%%', ''), sep)
+--     local file_path = ''
+--     for _, cur in ipairs(path_list) do
+--         file_path = (cur == '.' or cur == '~') and '' or
+--             file_path .. cur .. ' ' .. '%#LspSagaWinbarSep#>%*' .. ' %*'
+--     end
+--     return file_path .. file_name
+-- end
 
-local function config_winbar_or_statusline()
-    local exclude = {
-        ['terminal'] = true,
-        ['toggleterm'] = true,
-        ['prompt'] = true,
-        ['NvimTree'] = true,
-        ['help'] = true,
-    } -- Ignore float windows and exclude filetype
-    if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
-        vim.wo.winbar = ''
-    else
-        local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
-        local sym
-        if ok then sym = lspsaga.get_symbol_node() end
-        local win_val = ''
-        win_val = get_file_name(false) -- set to true to include path
-        if sym ~= nil then win_val = win_val .. sym end
-        vim.wo.winbar = ' ' .. win_val
-        -- if work in statusline
-        -- vim.wo.stl = win_val
-    end
-end
+-- local function config_winbar_or_statusline()
+--     local exclude = {
+--         ['terminal'] = true,
+--         ['toggleterm'] = true,
+--         ['prompt'] = true,
+--         ['NvimTree'] = true,
+--         ['help'] = true,
+--     } -- Ignore float windows and exclude filetype
+--     if vim.api.nvim_win_get_config(0).zindex or exclude[vim.bo.filetype] then
+--         vim.wo.winbar = ''
+--     else
+--         local ok, lspsaga = pcall(require, 'lspsaga.symbolwinbar')
+--         local sym
+--         if ok then sym = lspsaga.get_symbol_node() end
+--         local win_val = ''
+--         win_val = get_file_name(false) -- set to true to include path
+--         if sym ~= nil then win_val = win_val .. sym end
+--         vim.wo.winbar = ' ' .. win_val
+--         -- if work in statusline
+--         -- vim.wo.stl = win_val
+--     end
+-- end
 
-local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
+-- local events = { 'BufEnter', 'BufWinEnter', 'CursorMoved' }
 
-vim.api.nvim_create_autocmd(events, {
-    pattern = '*',
-    callback = function() config_winbar_or_statusline() end,
-})
+-- vim.api.nvim_create_autocmd(events, {
+--     pattern = '*',
+--     callback = function() config_winbar_or_statusline() end,
+-- })
 
-vim.api.nvim_create_autocmd('User', {
-    pattern = 'LspsagaUpdateSymbol',
-    callback = function() config_winbar_or_statusline() end,
-})
+-- vim.api.nvim_create_autocmd('User', {
+--     pattern = 'LspsagaUpdateSymbol',
+--     callback = function() config_winbar_or_statusline() end,
+-- })
