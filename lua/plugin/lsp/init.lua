fcf58175224bc5de18e8ad612ff7deeca28c3db4
Luco Bellic <luco.bellic@protonmail.com>
2023-01-12 21:01:57 +0100

    🔧 Add default lsp installation

--- lua/plugin/lsp/init.lua
+++ lua/plugin/lsp/init.lua
@@ -1,6 +1,19 @@
 local lsp_plugins = {
     { 'williamboman/mason.nvim', config = function() require('mason').setup() end },
-    { 'williamboman/mason-lspconfig.nvim', config = function() require('mason-lspconfig').setup() end },
+    { 'williamboman/mason-lspconfig.nvim',
+        config = function()
+                require('mason-lspconfig').setup { ensure_installed = {
+                    'jsonls',
+                    'vimls',
+                    'cmake',
+                    'sumneko_lua',
+                    'rust_analyzer',
+                    'pylsp',
+                    'pyright',
+                    'yapf'
+                }}
+        end
+    },
 
     { 'glepnir/lspsaga.nvim', config = function() require('plugin.lsp.saga') end },
 
