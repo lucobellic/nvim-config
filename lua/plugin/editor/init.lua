f7604b714315e244820d100ee7431183f65a75e7
Luco Bellic <luco.bellic@protonmail.com>
2023-01-21 15:21:36 +0100

    ➕ Add term-edit to use editor command in terminal

--- lua/plugin/editor/init.lua
+++ lua/plugin/editor/init.lua
@@ -3,12 +3,17 @@ local editor_plugins = {
     { 'junegunn/limelight.vim', config = function() vim.cmd('source ' .. config_path .. '/' .. 'limelight.vim') end }, -- Highlight paragraph
     { 'luochen1990/rainbow', config = function() vim.cmd('source ' .. config_path .. '/' .. 'rainbow.vim') end },
 
+    -- use term as in editor
+    { 'chomosuke/term-edit.nvim',
+        config = function() require('plugin.editor.term-edit') end
+    },
+
     -- smooth scroll
     { 'karb94/neoscroll.nvim', config = function() require('plugin.editor.neoscroll') end },
 
     { 'echasnovski/mini.indentscope', branch = 'stable', config = function() require('plugin.editor.indentscope') end },
     { 'sindrets/diffview.nvim',
-        dependencies = {'nvim-lua/plenary.nvim' } ,
+        dependencies = { 'nvim-lua/plenary.nvim' },
         config = function() require('plugin.editor.diffview') end
     },
 }
