90e5b94438dfa05a559e28b916f82b1ed179ea54
Luco Bellic <luco.bellic@protonmail.com>
2023-01-22 11:19:11 +0100

    ➖ Remove nvim-semantic-tokens
    Semantic tokens supported by neovim since 0.9

--- lua/plugin/lsp/config.lua
+++ lua/plugin/lsp/config.lua
@@ -58,19 +58,6 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("v", "<leader>=", "<cmd>lua vim.lsp.buf.format({async=true})<CR>", opts)
   end
 
-  if caps.semanticTokensProvider and caps.semanticTokensProvider.full then
-    local augroup = vim.api.nvim_create_augroup("SemanticTokens", {})
-    vim.api.nvim_create_autocmd("TextChanged", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.semantic_tokens_full()
-      end,
-    })
-    -- fire it first time on load as well
-    vim.lsp.buf.semantic_tokens_full()
-  end
-
 end
 
 -----------------------------
--- lua/plugin/lsp/init.lua
+++ lua/plugin/lsp/init.lua
@@ -10,7 +10,6 @@ local lsp_plugins = {
                     'rust_analyzer',
                     'pylsp',
                     'pyright',
-                    'yapf',
                     'clangd'
                 }}
         end
@@ -19,9 +18,7 @@ local lsp_plugins = {
     { 'glepnir/lspsaga.nvim', config = function() require('plugin.lsp.saga') end },
 
     --  native nvim lsp
-    { 'theHamsta/nvim-semantic-tokens', config = function() require('plugin.lsp.tokens') end },
     { 'neovim/nvim-lspconfig',
-        dependencies = { 'nvim-semantic-tokens' },
         config = function()
             require('plugin.lsp.config')
         end
