b69068a382e5b52b6b4ff4e7d58f564b761632e8
Luco Bellic <luco.bellic@protonmail.com>
2023-03-04 22:27:52 +0100

    ➕ Add mind.nvim

--- lua/plugin/navigation/init.lua
+++ lua/plugin/navigation/init.lua
@@ -1,8 +1,13 @@
 local navigation_plugins = {
-    { 'junegunn/fzf', run = 'fzf#install()' },
+    { 'junegunn/fzf',     run = 'fzf#install()' },
     { 'junegunn/fzf.vim', config = function() vim.cmd('source ' .. config_path .. '/' .. 'fzf.vim') end },
-
-    { 'phaazon/hop.nvim', branch = 'v2', config = function() require('plugin.navigation.hop') end },
+    {
+        'phaazon/hop.nvim',
+        branch = 'v2',
+        config = function()
+            require('plugin.navigation.hop')
+        end
+    },
     'tpope/vim-sensible',
     'tpope/vim-surround',
     'tpope/vim-commentary',
@@ -12,26 +17,37 @@ local navigation_plugins = {
     'tpope/vim-abolish',
 
     -- { 'kyazdani42/nvim-tree.lua', config = function() require('plugin.navigation.tree') end },
-    { 'nvim-neo-tree/neo-tree.nvim',
+    {
+        'nvim-neo-tree/neo-tree.nvim',
         branch = 'v2.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'MunifTanjim/nui.nvim',
             'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
         },
-        config = function ()
+        config = function()
             require('plugin.navigation.neo-tree')
         end
     },
 
     'tommcdo/vim-exchange',
     'tommcdo/vim-lion',
-
     { 'echasnovski/mini.ai', branch = 'stable', config = function() require('mini.ai').setup() end },
 
     -- Better matchup '%' usage
-    { 'andymass/vim-matchup',
-        config = function() vim.g.matchup_matchparen_offscreen = { method = 'status_manual' } end }
+    {
+        'andymass/vim-matchup',
+        config = function() vim.g.matchup_matchparen_offscreen = { method = 'status_manual' } end
+    },
+
+    {
+        'phaazon/mind.nvim',
+        branch = 'v2.2',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require 'mind'.setup()
+        end
+    }
 }
 
 return navigation_plugins
