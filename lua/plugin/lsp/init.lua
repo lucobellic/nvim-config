d950afd762414cdcd57ddea32478e09556514f25
Luco Bellic <luco.bellic@protonmail.com>
2023-03-15 21:39:19 +0100

    ✨ [mason] Add automatic installation

--- lua/plugin/lsp/init.lua
+++ lua/plugin/lsp/init.lua
@@ -1,34 +1,41 @@
 local lsp_plugins = {
-    { 'williamboman/mason.nvim', config = function() require('mason').setup() end },
-    { 'williamboman/mason-lspconfig.nvim',
+    {
+        'williamboman/mason.nvim',
         config = function()
-            require('mason-lspconfig').setup { ensure_installed = {
-                'jsonls',
-                'vimls',
-                'cmake',
-                'lua_ls',
-                'rust_analyzer',
-                'pylsp',
-                'pyright',
-                'clangd',
-            } }
+            require('mason').setup()
         end
     },
-
-    { 'glepnir/lspsaga.nvim', config = function() require('plugin.lsp.saga') end },
-
-    {'jose-elias-alvarez/null-ls.nvim'},
-
-    --  native nvim lsp
-    { 'neovim/nvim-lspconfig',
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function()
+            require('mason-lspconfig').setup {
+                ensure_installed = {
+                    'jsonls',
+                    'vimls',
+                    'cmake',
+                    'lua_ls',
+                    'rust_analyzer',
+                    'pylsp',
+                    'pyright',
+                    'clangd',
+                },
+                automatic_installation = true,
+            }
+        end
+    },
+    {
+        'glepnir/lspsaga.nvim',
+        config = function()
+            require('plugin.lsp.saga')
+        end
+    },
+    { 'jose-elias-alvarez/null-ls.nvim' },
+    {
+        'neovim/nvim-lspconfig',
         config = function()
             require('plugin.lsp.config')
         end
     },
-
-
-
-
 }
 
 return lsp_plugins
