a7db79a9396b6bf40301bbb0c4386669805ce064
Luco Bellic <luco.bellic@protonmail.com>
2023-03-31 22:37:34 +0200

    💄 [treesitter] Update rainbow colors

--- lua/plugin/ui/treesitter.lua
+++ lua/plugin/ui/treesitter.lua
@@ -1,7 +1,10 @@
-local cmd = vim.cmd
+vim.api.nvim_set_hl(0, 'Gold', { fg = "gold" })
+vim.api.nvim_set_hl(0, 'Orchid', { fg = "orchid" })
+vim.api.nvim_set_hl(0, 'LightSkyBlue', { fg = "LightSkyBlue" })
+vim.api.nvim_set_hl(0, 'DarkOrange', { fg = "DarkOrange" })
 
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = {"cmake", "python", "yaml", "json", "vim", "markdown"},
+  ensure_installed = { "cmake", "python", "yaml", "json", "vim", "markdown" },
   highlight = {
     enable = true,
     disable = { "c", "cpp", "rust", "lua" }, -- disable highlight supported by lsp
@@ -12,7 +15,15 @@ require 'nvim-treesitter.configs'.setup {
   },
   rainbow = {
     enable = true,
-    colors = { 'Gold', 'Orchid', 'LightSkyBlue', 'DarkOrange' }
+    hlgroups = {
+      'Gold',
+      'Orchid',
+      'LightSkyBlue',
+      'DarkOrange',
+      'TSRainbowGreen',
+      'TSRainbowViolet',
+      'TSRainbowCyan'
+    },
   }
 }
 
