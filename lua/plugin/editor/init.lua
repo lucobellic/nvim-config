7af6f87aa6a5eb447d54c2af30d78bf93405a817
Luco Bellic <luco.bellic@protonmail.com>
2023-03-31 22:48:08 +0200

    ➖ Remove luochen1990/rainbow

--- lua/plugin/editor/init.lua
+++ lua/plugin/editor/init.lua
@@ -1,7 +1,6 @@
 local editor_plugins = {
     { 'folke/zen-mode.nvim',    config = function() require('plugin.editor.zenmode') end },
     { 'junegunn/limelight.vim', config = function() vim.cmd('source ' .. config_path .. '/' .. 'limelight.vim') end }, -- Highlight paragraph
-    { 'luochen1990/rainbow',    config = function() vim.cmd('source ' .. config_path .. '/' .. 'rainbow.vim') end },
 
     -- use term as in editor
     {
