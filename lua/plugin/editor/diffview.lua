664e35bf288d9b6522cfa2b1bb509e1ebde385a1
Luco Bellic <luco.bellic@protonmail.com>
2023-04-15 09:19:28 +0200

    ⚰️  Remove color settings supported by colorscheme

--- lua/plugin/editor/diffview.lua
+++ lua/plugin/editor/diffview.lua
@@ -1,9 +1,3 @@
-local actions = require("diffview.actions")
-
--- colors
-vim.cmd("hi! link DiffviewDiffDelete Comment")
-vim.cmd("hi! link DiffviewDiffAddAsDelete Comment")
-
 require("diffview").setup({
   diff_binaries = false, -- Show diffs for binaries
   enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
