24a2c07f83ddf4989de2699e2015e8f6103b08b6
Luco Bellic <luco.bellic@protonmail.com>
2023-04-21 22:11:53 +0200

    🔥 Remove neotree highlights

--- lua/colors.lua
+++ lua/colors.lua
@@ -4,6 +4,4 @@ vim.api.nvim_set_hl(0, "Transparent", { fg = normal_bg, bg = 'none', blend = 0 }
 vim.api.nvim_set_hl(0, "OutlineIndent", { fg = "#FF8F40" })
 vim.api.nvim_set_hl(0, "SagaShadow"   , { bg = "#000000" })
 
-vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { link = 'NeoTreeDotfile' })
-vim.api.nvim_set_hl(0, "NeoTreeGitUnstaged",  { link = 'NeoTreeDotfile' })
 
