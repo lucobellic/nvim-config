497bf3c8d80be48988d003fea5aeea1477750e1e
Luco Bellic <luco.bellic@protonmail.com>
2023-04-21 22:09:24 +0200

    🎨 [gitsigns] Format gitsigns

--- lua/plugin/ui/gitsigns.lua
+++ lua/plugin/ui/gitsigns.lua
@@ -1,16 +1,20 @@
 require('gitsigns').setup {
-  signs                             = {
+
+  signs = {
     add          = { hl = 'GitSignsAdd', text = '▕', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
     change       = { hl = 'GitSignsChange', text = '▕', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
     delete       = { hl = 'GitSignsDelete', text = '▕', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     topdelete    = { hl = 'GitSignsDelete', text = '▕', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
     changedelete = { hl = 'GitSignsChange', text = '▕', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
-  signcolumn                        = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl                             = false, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
-  keymaps                           = {
+
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+
+  keymaps = {
+
     -- Default keymap options
     noremap = true,
     silent = true,
@@ -34,25 +38,29 @@ require('gitsigns').setup {
     ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
     ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
   },
-  watch_gitdir                      = {
+  watch_gitdir = {
     interval = 1000,
     follow_files = true
   },
-  attach_to_untracked               = true,
-  current_line_blame                = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts           = {
+  attach_to_untracked = true,
+  current_line_blame  = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+
+  current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
   },
+
   current_line_blame_formatter_opts = {
     relative_time = false
   },
-  sign_priority                     = 30,
-  update_debounce                   = 100,
-  status_formatter                  = nil, -- Use default
-  max_file_length                   = 40000,
-  preview_config                    = {
+
+  sign_priority    = 30,
+  update_debounce  = 100,
+  status_formatter = nil, -- Use default
+  max_file_length  = 40000,
+
+  preview_config = {
     -- Options passed to nvim_open_win
     border = 'single',
     style = 'minimal',
@@ -60,38 +68,13 @@ require('gitsigns').setup {
     row = 0,
     col = 1
   },
-  yadm                              = {
+
+  yadm = {
     enable = false
   },
+
   -- Solve CRLF issue with gitsigns
-  diff_opts                         = {
+  diff_opts = {
     internal = false
   }
 }
--- gs.setup {
--- signs = {
---     add = {hl = "DiffAdd", text = '▏', numhl = "GitSignsAddNr"},
---     change = {hl = "DiffChange", text = '▏', numhl = "GitSignsChangeNr"},
---     delete = {hl = "DiffDelete", text = '▏', numhl = "GitSignsDeleteNr"},
---     topdelete = {hl = "DiffDelete", text = '▏', numhl = "GitSignsDeleteNr"},
---     changedelete = {hl = "DiffChange", text = '▏', numhl = "GitSignsChangeNr"}
--- },
--- numhl = false,
--- keymaps = {
---     -- Default keymap options
---     noremap = true,
---     buffer = true,
---     ["n >h"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
---     ["n <h"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''},
---     ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
---     ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
---     ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
---     ["n <leader>hv"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
---     ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
--- },
--- watch_index = {
---     interval = 100
--- },
--- sign_priority = 5,
--- status_formatter = nil -- Use default
--- }
