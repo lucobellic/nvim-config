48e849222d12a1e2864920bd876fbe12204aae12
Luco Bellic <luco.bellic@protonmail.com>
2023-05-03 17:45:41 +0200

    🔨 Manually load configuration files

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = ' '
 
+require("config.filetype")
+require("config.neovide")
+require("config.options")
+require("config.shell")
+
 require("config.lazy")
 require("config.keymaps")
-require("config.options")
