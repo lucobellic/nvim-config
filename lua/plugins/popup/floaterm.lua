3465e2afc293ae63dd634a31efd94307646cd131
Luco Bellic <luco.bellic@protonmail.com>
2023-05-14 16:34:27 +0200

    ♻️  [floaterm] re-write ToggleTool in lua

--- lua/plugins/popup/floaterm.lua
+++ lua/plugins/popup/floaterm.lua
@@ -1,43 +1,21 @@
--- TODO: Replace this vim code to lua
-vim.cmd [[
-function! GetBufnrUnnamed() abort
-  let buflist = floaterm#buflist#gather()
-  for bufnr in buflist
-    let name = getbufvar(bufnr, 'floaterm_name')
-    if empty(name)
-      return bufnr
-    endif
-  endfor
-  return -1
-endfunction
-
-function! ToggleTool(tool, count) abort
-  " find bufnr according to the tool name
-  let bufnr = empty(a:tool) ?
-    \ GetBufnrUnnamed() : floaterm#terminal#get_bufnr(a:tool)
-
-  if bufnr == -1
-    if empty(a:tool)
-      " ToggleTool should only be called from
-      " normal mode or terminal mode without bang
-      call floaterm#run('new', 0, [visualmode(), 0, 0, 0], '')
-    else
-      call floaterm#run('new', 0, [visualmode(), 0, 0, 0],
-        \ printf('--height=0.8 --width=0.8 --title=%s\ $1/$2 --name=%s %s', a:tool, a:tool, a:tool))
-    endif
+local function floaterm_toogle(params)
+  local tool_name = params.args
+  local bufnr = vim.api.nvim_call_function('floaterm#terminal#get_bufnr', { tool_name })
+  if bufnr == -1 then
+    local format = string.format(
+      '--height=0.8 --width=0.8 --title=%s\\ $1/$2 --name=%s %s',
+      tool_name,
+      tool_name,
+      tool_name
+    )
+    vim.api.nvim_call_function('floaterm#run', { 'new', tool_name, { 'v', 0, 0, 0 }, format })
   else
-    call floaterm#toggle(0, a:count ? a:count : bufnr, '')
-    " workaround to prevent lazygit shift left;
-    " another workaround here is to use sidlent!
-    " to ignore can't re-enter normal mode error
-    execute('silent! normal! 0')
-  endif
-endfunction
+    vim.api.nvim_call_function('floaterm#toggle', { 0, bufnr, '' })
+  end
+end
 
-command! -nargs=? -count=0 ToggleTool call ToggleTool(<q-args>, <count>)
-nnoremap <silent> <leader>g; <Cmd>execute v:count . 'ToggleTool lazygit'<CR>
-tnoremap <silent> <leader>g; <Cmd>execute v:count . 'ToggleTool lazygit'<CR>
-]]
+vim.api.nvim_create_user_command('ToggleTool', floaterm_toogle, { nargs = 1, count = 1 })
+vim.api.nvim_set_keymap('n', '<leader>g;', ':ToggleTool lazygit<cr>', { noremap = true, silent = true })
 
 vim.g.floaterm_shell = vim.o.shell
 
