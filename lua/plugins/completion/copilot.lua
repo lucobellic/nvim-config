0f5e091e84359d283d5f9166fba38e2e33879d40
Luco Bellic <luco.bellic@protonmail.com>
2023-05-19 13:28:11 +0200

    👽 [copilot] Update copilot configuration

--- lua/plugins/completion/copilot.lua
+++ lua/plugins/completion/copilot.lua
@@ -1,4 +1,17 @@
+local cmd_output = vim.fn.systemlist('node --version')
+local node_version = string.match(cmd_output[#cmd_output], "^v(%S+)") or ""
+require('copilot.client').node_version = node_version
+
 require("copilot").setup({
-  suggestion = { enabled = false },
-  panel = { enabled = false },
+  filetypes = {
+    yaml = false,
+    markdown = true,
+    help = false,
+    gitcommit = true,
+    gitrebase = true,
+    hgcommit = true,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
 })
