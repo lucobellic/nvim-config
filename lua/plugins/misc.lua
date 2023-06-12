a26a27d13a63836ae38df8e0d08fec0f90acf4c9
Luco Bellic <luco.bellic@protonmail.com>
2023-06-12 11:43:47 +0200

    ➕ Add markmap for markdown mindmaps visualization

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -11,5 +11,17 @@ return {
   -- Other
   { 'moll/vim-bbye',       event = 'VeryLazy' },     -- Close buffer and window
   { 'xolox/vim-misc',      event = 'VeryLazy' },
-  { 'honza/vim-snippets',  event = 'VeryLazy' }
+  { 'honza/vim-snippets',  event = 'VeryLazy' },
+  -- Visualize Markdown as mindmaps
+  {
+    "Zeioth/markmap.nvim",
+    build = "yarn global add markmap-cli",
+    cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+    opts = {
+      html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+      hide_toolbar = false,              -- (default)
+      grace_period = 3600000             -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+    },
+    config = function(_, opts) require("markmap").setup(opts) end
+  },
 }
