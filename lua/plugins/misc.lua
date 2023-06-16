f48f73aaf36fa39a7d8deb41c06e0e2d613cbc98
Luco Bellic <luco.bellic@protonmail.com>
2023-06-16 17:53:05 +0200

    ➕ Add neorg plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -24,4 +24,26 @@ return {
     },
     config = function(_, opts) require("markmap").setup(opts) end
   },
+
+  {
+    'nvim-neorg/neorg',
+    event = 'VeryLazy',
+    build = ':Neorg sync-parsers',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},  -- Loads default behavior
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+          ['core.dirman'] = {      -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = '~/notes',
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
 }
