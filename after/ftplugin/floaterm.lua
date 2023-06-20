18820af023d8e99aac91fc0e16f68cf7ae36018a
Luco Bellic <luco.bellic@protonmail.com>
2023-06-20 12:56:10 +0200

    ✨ [floaterm] Set better floaterm close keymap
    This close keymap switch to previous floaterm after closing the current
    one. Provide same behavior as closing file tab

--- after/ftplugin/floaterm.lua
+++ after/ftplugin/floaterm.lua
@@ -1,10 +1,7 @@
 local opts = { silent = true, noremap = true }
 
 vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':FloatermHide<CR>', opts)
-vim.api.nvim_buf_set_keymap(0, 'n', '<esc>', ':FloatermHide<CR>', opts)
-
-vim.api.nvim_buf_set_keymap(0, 'n', '<C-q>', ':FloatermKill<CR>', opts)
-vim.api.nvim_buf_set_keymap(0, 't', '<C-q>', '<C-\\><C-n>:FloatermKill<CR>', opts)
+vim.api.nvim_buf_set_keymap(0, 'n', '<esc>', ':FloatermHide<CR><esc>', opts)
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<C-l>', ':FloatermNext<CR>', opts)
 vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', '<C-\\><C-n>:FloatermNext<CR>', opts)
@@ -29,10 +26,28 @@ local function open_in_normal_window()
   end
 end
 
+local function close_current_floaterm()
+  local buffer = vim.api.nvim_call_function('floaterm#buflist#curr', {})
+  if buffer > 0 then
+    vim.api.nvim_call_function('floaterm#terminal#kill', { buffer })
+    vim.api.nvim_call_function('floaterm#prev', {})
+  else
+    vim.notify('No floaterm to close', vim.log.levels.WARN)
+  end
+end
+
 vim.api.nvim_create_user_command(
   'OpenInNormalWindow',
   open_in_normal_window,
   { desc = 'Open file in normal window from floating terminal' }
 )
 
+vim.api.nvim_create_user_command(
+  'FloatermCloseCurrent',
+  close_current_floaterm,
+  { desc = 'Close current floaterm' }
+)
+
 vim.api.nvim_buf_set_keymap(0, 'n', 'gf', ':OpenInNormalWindow<cr>', { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-q>', ':FloatermCloseCurrent<cr>', { noremap = true, silent = true })
+vim.api.nvim_buf_set_keymap(0, 't', '<C-q>', '<C-\\><C-n>:FloatermCloseCurrent<cr>', { noremap = true, silent = true })
