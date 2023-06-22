66717f40bced4ca1f9e43e23f497b557019d316d
Luco Bellic <luco.bellic@protonmail.com>
2023-06-22 16:42:44 +0200

    ➖ Replace auto-save by auto command
    auto-save is buggy and unmaintained
    also it is not needed for such simple use case

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -15,3 +15,21 @@ vim.api.nvim_create_autocmd({ 'WinLeave' }, {
   command = 'setlocal nocursorline',
   desc = 'Hide cursorline when leaving window',
 })
+
+-- Automatic save
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
+  pattern = '*',
+  callback = function(ev)
+    local modified = vim.bo.modifiable and vim.bo.modified
+    local not_popup = vim.fn.pumvisible() == 0 -- or vim.api.nvim_win_get_config(0).zindex
+    if modified and not_popup then
+      -- Save and restore cursor position
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      -- Remove trailing whitespace
+      vim.cmd('silent! %s/\\s\\+$//e')
+      vim.cmd('silent! write')
+      vim.api.nvim_win_set_cursor(0, cursor)
+    end
+  end,
+  desc = 'Automatic save after insertion leave',
+})
--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -101,34 +101,6 @@ local editor_plugins = {
       require('plugins.editor.yanky')
     end
   },
-  {
-    -- 'Pocco81/auto-save.nvim',
-    -- NOTE: Temporary replacement until [#67](https://github.com/Pocco81/auto-save.nvim/pull/67) is merged
-    'zoriya/auto-save.nvim',
-    event = 'VeryLazy',
-    config = function()
-      require("auto-save").setup {
-        print_enabled = false,
-        callbacks = {
-          before_saving = function()
-            -- Remove trailing whitespace
-            local buf = vim.api.nvim_get_current_buf()
-            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-            for i, line in ipairs(lines) do
-              local new_line = line:gsub("%s+$", "")
-              if new_line ~= line then
-                vim.api.nvim_buf_set_lines(buf, i - 1, i, false, { new_line })
-              end
-            end
-          end,
-          enabling = nil,              -- ran when enabling auto-save
-          disabling = nil,             -- ran when disabling auto-save
-          before_asserting_save = nil, -- ran before checking `condition`
-          after_saving = nil           -- ran after doing the actual save
-        }
-      }
-    end,
-  }
 }
 
 return editor_plugins
