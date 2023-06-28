78db40d3149ae5d66567fdb51d68f6ef68d81504
Luco Bellic <luco.bellic@protonmail.com>
2023-06-28 20:52:45 +0200

    💄 [diagnostic] Remove line highlight for info

--- lua/plugins/lsp/config/misc.lua
+++ lua/plugins/lsp/config/misc.lua
@@ -38,6 +38,6 @@ end
 vim.cmd [[
   sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
   sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=DiagnosticLineNrWarn
-  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=DiagnosticLineNrInfo
-  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=DiagnosticLineNrHint
+  sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=
+  sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=
 ]]
