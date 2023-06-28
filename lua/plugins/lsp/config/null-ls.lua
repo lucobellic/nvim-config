8ad0caf68f5fd0bb3e8ad0c8a1928658be85c322
Luco Bellic <luco.bellic@protonmail.com>
2023-06-28 20:53:31 +0200

    ✨ [null-ls] Enable cspell with null-ls

--- lua/plugins/lsp/config/null-ls.lua
+++ lua/plugins/lsp/config/null-ls.lua
@@ -2,13 +2,21 @@ local util = require('plugins.lsp.config.util')
 
 -- null-ls
 local null_ls = require("null-ls")
+local cspell_config = {
+  find_json = function(cwd)
+    return vim.fn.stdpath("config") .. '/spell/cspell.json'
+  end
+}
+
 local cspell = require('cspell')
 null_ls.setup({
   on_attach = util.on_attach,
   fallback_severity = vim.diagnostic.severity.INFO,
   sources = {
     null_ls.builtins.diagnostics.rstcheck,
-    cspell.diagnostics,
-    cspell.code_actions,
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.formatting.prettier,
+    cspell.diagnostics.with({ config = cspell_config }),
+    cspell.code_actions.with({ config = cspell_config }),
   },
 })
