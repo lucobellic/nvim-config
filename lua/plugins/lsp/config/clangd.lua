8c481363027178d98bc4fbc9419be934f13a8376
Luco Bellic <luco.bellic@protonmail.com>
2023-06-28 20:54:29 +0200

    🔧 [lsp] Configure clangd setup

--- lua/plugins/lsp/config/clangd.lua
+++ lua/plugins/lsp/config/clangd.lua
@@ -1,15 +1,17 @@
 -- C/C++ Clangd configuration
+local nvim_lsp = require('lspconfig')
 local util = require('plugins.lsp.config.util')
 local Path = require('plenary.path')
 
 local cmd_output = vim.fn.systemlist('clangd --version')
 local clangd_version = string.match(cmd_output[2], "version (%d+)") or '13'
-vim.print("Using clangd version: " .. clangd_version)
 
 local clangd_args = {
   ['16'] = {
     "--background-index",
     "--background-index-priority=background",
+    "--all-scopes-completion",
+    "--pch-storage=memory",
     "--clang-tidy",
     "--enable-config",
     "--header-insertion=iwyu",
@@ -32,22 +34,21 @@ local clangd_capabilities = vim.tbl_extend('force',
   {
     offsetEncoding = { "utf-16" },
     textDocument = {
-      completion = {
-        editsNearCursor = true }
+      inlayHints = { enabled = true },
+      completion = { editsNearCursor = true }
     }
   }
 )
 
-require('clangd_extensions').setup({
-  server = {
-    on_attach = util.on_attach,
-    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-    settings = {
-      arguments = clangd_args[clangd_version],
-      semanticHighlighting = true
-    },
-    capabilities = clangd_capabilities
-  }
+nvim_lsp.clangd.setup({
+  cmd = { mason_clangd_path }, -- { '/usr/bin/clangd' },
+  on_attach = util.on_attach,
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  capabilities = clangd_capabilities,
+  settings = {
+    arguments = clangd_args[clangd_version],
+    semanticHighlighting = true,
+  },
 })
 
 -- TODO: set it in clangd.setup
