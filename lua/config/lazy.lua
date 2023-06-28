6e595be8fc8bda1a1bed08309608ac135de50442
Luco Bellic <luco.bellic@protonmail.com>
2023-06-28 20:42:00 +0200

    🔧 [lazy] Disable auto update and refresh

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,15 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
@@ -21,7 +26,12 @@ require('lazy').setup({
     border = 'rounded',
   },
   install = { colorscheme = { 'tokyonight', 'habamax' } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
