780568bb21a4f40cd2461d39fb9f3c9461ce7240
Luco Bellic <luco.bellic@protonmail.com>
2023-06-29 12:31:36 +0200

    ➕ Add LazyVim plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   spec = {
     { import = 'plugins' },
+    { "LazyVim/LazyVim", import = 'lazyvim.plugins' },
   },
   defaults = {
     lazy = false,
