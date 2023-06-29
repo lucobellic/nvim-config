ea879f8bd03030b6fa93e97d707f5bac12b8f366
Luco Bellic <luco.bellic@protonmail.com>
2023-06-29 12:36:50 +0200

    🔥 [lazyvim] Remove automatically loaded configs

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,8 +1,4 @@
-require('config.autocmd')
 require('config.filetype')
 require('config.neovide')
-require('config.options')
 require('config.shell')
-
-require('config.lazy')
-require('config.keymaps')
+require("config.lazy")
