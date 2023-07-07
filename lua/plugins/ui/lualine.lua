ef0dc4d6d8fa2fed47779f8331b59b5766ab9d2b
Luco Bellic <luco.bellic@protonmail.com>
2023-07-07 22:53:05 +0200

    ➕ Add lualine with configuration

---
+++ lua/plugins/ui/lualine.lua
@@ -0,0 +1,44 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    options = {
+      theme = 'auto',
+      component_separators = '|',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { { 'branch', icon = '' } },
+      lualine_b = {
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+        },
+      },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {
+        -- stylua: ignore
+        {
+          'progress',
+          separator = ' ',
+          padding = { left = 1, right = 0 }
+        },
+        {
+          'location',
+          padding = { left = 0, right = 1 }
+        },
+      },
+      lualine_z = {
+        function()
+          return '  ' .. os.date('%R')
+        end,
+      },
+    },
+  }
+}
