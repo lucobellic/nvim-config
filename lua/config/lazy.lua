33ef336c167fc1be3d046fe38dfe13a28e9b95b6
Luco Bellic <luco.bellic@protonmail.com>
2023-07-08 21:03:29 +0200

    ➕ [lazy] Add dap plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,15 +13,17 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   spec = {
-    { "LazyVim/LazyVim",                                    import = 'lazyvim.plugins' },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { "LazyVim/LazyVim", import = 'lazyvim.plugins' },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.test.core" },
+
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = 'lazyvim.plugins.extras.coding.copilot' },
