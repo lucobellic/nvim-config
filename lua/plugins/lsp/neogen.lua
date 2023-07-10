1d522011f19ec78150ff893579ec11567f736db1
Luco Bellic <luco.bellic@protonmail.com>
2023-07-11 01:15:58 +0200

    🔧 [neogen] Refactor keymaps

--- lua/plugins/lsp/neogen.lua
+++ lua/plugins/lsp/neogen.lua
@@ -3,6 +3,10 @@ return
   'danymat/neogen',
   event = 'VeryLazy',
   dependencies = 'nvim-treesitter/nvim-treesitter',
+  keys = {
+    { '<leader>nf', function() require("neogen").generate({}) end,                 desc = 'Document' },
+    { '<leader>nc', function() require('neogen').generate({ type = 'class' }) end, desc = 'Document class' },
+  },
   opts = {
     snippet_engine = 'luasnip',
     input_after_comment = false,
