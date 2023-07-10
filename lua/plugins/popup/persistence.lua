64ae273b8efb10bd44afff5bf1df5480fdd4eb81
Luco Bellic <luco.bellic@protonmail.com>
2023-07-11 01:03:38 +0200

    🔧 [persistence] Refactor keymaps

--- lua/plugins/popup/persistence.lua
+++ lua/plugins/popup/persistence.lua
@@ -1,5 +1,11 @@
 return {
   'folke/persistence.nvim',
+  keys = {
+    { '<leader>qr', function() require("persistence").load() end,                desc = 'Restore session' },
+    { '<leader>ql', function() require("persistence").load({ last = true }) end, desc = 'Load last session' },
+    { '<leader>qd', function() require("persistence").stop() end,                desc = 'Stop session' },
+    { '<leader>qs', ':PersistenceLoadSession<cr>',                               desc = 'Load session' },
+  },
   opts = {
     options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" },
     pre_save = function() vim.api.nvim_exec_autocmds('User', { pattern = 'SessionSavePre' }) end
