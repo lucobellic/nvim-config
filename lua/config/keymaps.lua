beb82198f35d51ff25182af2dd67ce9a98f8e341
Luco Bellic <luco.bellic@protonmail.com>
2023-07-11 01:18:26 +0200

    🔧 [keymaps] Remove duplicated maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.del('n', '<leader>ud')
 vim.keymap.del('n', '<leader>gg')
 vim.keymap.del('n', '<leader>gG')
 
-local wk_ok, wk = pcall(require, 'which-key')
+local wk = require('which-key')
 
 -- NOTE: Set <c-f> keymap once again due to configuration issue
 vim.keymap.set(
@@ -21,7 +21,7 @@ vim.keymap.set(
 )
 
 -- Toggle
-vim.keymap.set("n", "<leader>ub", ':Gitsigns toggle_current_line_blame<cr>', { desc = "Toggle Line Blame" })
+wk.register({ ['<leader>ut'] = { ':TransparencyToggle<cr>', 'Toggle Transparency' } })
 wk.register({
   ['<leader>ud'] = {
     name = 'Toggle Diagnostics',
@@ -37,6 +37,7 @@ vim.keymap.set('n', '<leader>qa', '<leader>qq', { remap = true, desc = 'Quit all
 vim.api.nvim_set_keymap('c', '<esc>', '<C-c>', opts)
 vim.api.nvim_set_keymap('t', '<esc>', '<C-\\><C-n>', opts)
 vim.api.nvim_set_keymap('n', '<leader>w', '<C-w>', { noremap = false })
+vim.keymap.set('n', '<C-w>q', '<C-w>c', { desc = 'Delete window', remap = true })
 
 -- Spelling
 vim.api.nvim_set_keymap('n', '>S', ']s', opts)
@@ -44,32 +45,16 @@ vim.api.nvim_set_keymap('n', '>s', ']s', opts)
 vim.api.nvim_set_keymap('n', '<S', '[s', opts)
 vim.api.nvim_set_keymap('n', '<s', '[s', opts)
 
-
-vim.keymap.set('n', '<leader>wq', '<C-W>c', { desc = 'Delete window', remap = true })
-
 -- vim.api.nvim_set_keymap('v', '<leader>fw', "\"sy:Telescope live_grep default_text=<C-r>=substitute(substitute(escape(substitute(@s, '\\', '\\\\\\', 'g'), ' '), '\n', '', 'g'), '/', '\\/', 'g')\"<cr><cr>", opts)
 vim.api.nvim_set_keymap('v', '/', '"hy/<C-r>h', { silent = false, noremap = true })
 
 -- Tab navigation
-if wk_ok then
-  wk.register({
-    ["<C-k>"] = { ':tabnext<cr>', 'Next Tab' },
-    ["<C-j>"] = { ':tabprev<cr>', 'Previous Tab' },
-    g = {
-      n = { ':tabnew<cr>', 'New Tab' },
-      q = { ':tabclose<cr>', 'Close Tab' },
-    }
-  })
-end
-
-vim.keymap.set('n', '<C-b>', '<cmd>Neotree toggle reveal_force_cwd<cr>', opts)
-
--- search current word
-vim.keymap.set({ "n" }, "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>")
-vim.keymap.set({ "v" }, "<leader>s", "<esc>:lua require('spectre').open_visual()<CR>")
-
--- search in current file
-vim.keymap.set({ "n", "x" }, "<leader>sp", "<cmd>lua require('spectre').open_file_search()<cr>")
+vim.keymap.del({ 'n', 't' }, '<C-k>')
+vim.keymap.del({ 'n', 't' }, '<C-j>')
+vim.keymap.set('n', '<C-k>', ':tabnext<cr>', { desc = 'Tab Next' })
+vim.keymap.set('n', '<C-j>', ':tabprev<cr>', { desc = 'Tab Prev' })
+vim.keymap.set('n', 'gn', ':tabnew<cr>', { desc = 'Tab New' })
+vim.keymap.set('n', 'gq', ':tabclose<cr>', { desc = 'Tab Close' })
 
 -- Copilot
 wk.register({ ['<leader>cp'] = { ':Copilot panel<cr>', 'Copilot Panel' } })
