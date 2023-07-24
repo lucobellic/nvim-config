c7bf4098ba38ab347d82eda0b7ad074541aa3750
Luco Bellic <luco.bellic@protonmail.com>
2023-07-24 20:02:32 +0200

    ➕ [lazy] Add cmake and python semshi highlight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,9 +16,11 @@ require('lazy').setup({
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
     { import = 'lazyvim.plugins.extras.coding.yanky' },
 
+    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = 'lazyvim.plugins.extras.lang.clangd' },
     { import = 'lazyvim.plugins.extras.lang.docker' },
     { import = 'lazyvim.plugins.extras.lang.python' },
+    { import = "lazyvim.plugins.extras.lang.python-semshi" },
     { import = 'lazyvim.plugins.extras.lang.rust' },
     -- { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.json' },
