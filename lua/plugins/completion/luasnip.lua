4aab813a44e23a0781b5f253aaecbdacece28736
Luco Bellic <luco.bellic@protonmail.com>
2023-07-29 14:48:41 +0200

    ✨ [luasnip] Add friendly-snippet with c++

--- lua/plugins/completion/luasnip.lua
+++ lua/plugins/completion/luasnip.lua
@@ -1,7 +1,13 @@
 -- Disable default keymaps
 return {
-  "L3MON4D3/LuaSnip",
+  'L3MON4D3/LuaSnip',
+  dependencies = { 'rafamadriz/friendly-snippets' },
   keys = function()
     return {}
   end,
+  config = function(_, opts)
+    require('luasnip').filetype_extend('c', { 'cdoc' })
+    require('luasnip').filetype_extend('cpp', { 'cppdoc' })
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end
 }
