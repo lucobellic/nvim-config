d221f11e9027d659a2f020299dc394d3e0e2647d
Luco Bellic <luco.bellic@protonmail.com>
2023-10-23 18:56:25 +0200

    ✨ [flash] Filter folds for label 2 jump

--- lua/plugins/navigation/flash.lua
+++ lua/plugins/navigation/flash.lua
@@ -1,4 +1,5 @@
 local Flash = require("flash")
+local flash_search = require("flash.search")
 
 ---@param opts Flash.Format
 local function format(opts)
@@ -9,8 +10,16 @@ local function format(opts)
   }
 end
 
+---@param match Flash.Match
+local function add_fold(match)
+  vim.api.nvim_win_call(match.win, function()
+    local fold = vim.fn.foldclosed(match.pos[1])
+    match.fold = fold ~= -1 and fold or nil
+  end)
+end
+
 local function label2_jump(multi_window, pattern)
-  require('flash').jump({
+  Flash.jump({
     search = { mode = "search", multi_window = multi_window },
     label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
     pattern = pattern,
@@ -32,6 +41,30 @@ local function label2_jump(multi_window, pattern)
           end
         end,
       })
+    end,
+    matcher = function(win, _state, _opts)
+      local Search = require("flash.search")
+      local search = Search.new(win, _state)
+      local matches = {} ---@type Flash.Match[]
+      local folds = {} ---@type table<number, boolean>
+
+      for _, match in ipairs(search:get(_opts)) do
+        local skip = false
+        add_fold(match)
+
+        -- Only label the first match in each fold
+        if not skip and match.fold then
+          if folds[match.fold] then
+            skip = true
+          else
+            folds[match.fold] = true
+          end
+        end
+        if not skip then
+          table.insert(matches, match)
+        end
+      end
+      return matches
     end,
     labeler = function(matches, state)
       local labels = state:labels()
