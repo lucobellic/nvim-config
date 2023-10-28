74ea660526ebc488c34f8b5a423dee2b7849d0c3
Luco Bellic <luco.bellic@protonmail.com>
2023-10-28 19:07:54 +0200

    💄 [notify] Add fade in slide out effect

--- lua/plugins/popup/notify.lua
+++ lua/plugins/popup/notify.lua
@@ -4,10 +4,10 @@ return {
     background_colour = "NotifyBackground",
     fps = 30,
     level = 2,
-    minimum_width = 50,
-    max_width = 100,
+    minimum_width = 80,
+    max_width = 80,
     max_height = 5,
-    render = 'compact', -- 'default', 'minimal', 'simple', 'compact'
+    render = 'wrapped-compact', -- 'default', 'minimal', 'simple', 'compact', 'wrapped-compact'
     stages = require('util.notify.limited_slide'),
     timeout = 1000,
     top_down = false
