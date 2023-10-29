3c1c7dafad9bdcaa0f8c4c73f1df2483a438dffd
Luco Bellic <luco.bellic@protonmail.com>
2023-10-29 12:06:44 +0100

    🔧 [edgy] Set edgy for outline and overseer

--- lua/plugins/ui/edgy.lua
+++ lua/plugins/ui/edgy.lua
@@ -2,7 +2,10 @@
 return {
   'folke/edgy.nvim',
   event = 'VeryLazy',
-  enabled = false,
+  keys = {
+    { '<leader>wl', function() require('edgy').toggle('right') end, desc = 'Edgy Close Right' },
+    { '<leader>wh', function() require('edgy').toggle('left') end,  desc = 'Edgy Close Left' }
+  },
   opts = {
     close_when_all_hidden = false,
     icons = {
@@ -32,7 +35,24 @@ return {
         pinned = true,
         open = 'OverseerOpen',
       }
-    }
+    },
+    left = {
+      -- {
+      --   title = "Neo-Tree",
+      --   ft = "neo-tree",
+      --   pinned = true,
+      --   filter = function(buf)
+      --     return vim.b[buf].neo_tree_source == "filesystem"
+      --   end,
+      --   size = { height = 0.5 },
+      -- },
+    },
+    animate = {
+      cps = 300,
+      spinner = {
+        frames = { "", "", "", "", "", "" },
+      },
+    },
   }
 }
 -- return {
