1b3090a23c183bf2958eb4d1941998eaf3aa958e
Luco Bellic <luco.bellic@protonmail.com>
2023-10-29 11:49:56 +0100

    💄 [satellite] Set icon priority

--- lua/plugins/ui/satellite.lua
+++ lua/plugins/ui/satellite.lua
@@ -3,6 +3,7 @@ return {
   event = 'BufEnter',
   opts = {
     current_only = true,
+    excluded_filetypes = {'OverseerList'},
     handlers = {
       cursor = {
         enable = true,
@@ -10,6 +11,7 @@ return {
       },
       search = {
         enable = true,
+        priority = 55 -- Below cursor and above diagnostic
       },
       diagnostic = {
         enable = true,
