1269db0a8bf4e62e51ea8a040eb3327fb815f72c
Luco Bellic <luco.bellic@protonmail.com>
2023-10-29 12:09:38 +0100

    💄 [notify] Reduce the size of notify popup

--- lua/plugins/popup/notify.lua
+++ lua/plugins/popup/notify.lua
@@ -4,8 +4,8 @@ return {
     background_colour = "NotifyBackground",
     fps = 30,
     level = 2,
-    minimum_width = 80,
-    max_width = 80,
+    minimum_width = 60,
+    max_width = 60,
     max_height = 5,
     render = 'wrapped-compact', -- 'default', 'minimal', 'simple', 'compact', 'wrapped-compact'
     stages = require('util.notify.limited_slide'),
