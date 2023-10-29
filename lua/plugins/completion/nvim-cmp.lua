e07bff3c036aec2feb4c54b45aed2a31934a84fa
Luco Bellic <luco.bellic@protonmail.com>
2023-10-29 11:58:32 +0100

    🔧 [nvim-cmp] Update cmp priority

--- lua/plugins/completion/nvim-cmp.lua
+++ lua/plugins/completion/nvim-cmp.lua
@@ -123,10 +123,10 @@ return {
     sources = {
       { name = 'copilot',                 group_index = 1 },
       { name = 'nvim_lsp',                group_index = 1 },
-      { name = 'luasnip',                 group_index = 1 },
-      { name = 'buffer',                  group_index = 1 },
-      { name = 'path',                    group_index = 1 },
-      { name = 'nvim_lsp_signature_help', group_index = 2 },
+      { name = 'luasnip',                 group_index = 2 },
+      { name = 'buffer',                  group_index = 2 },
+      { name = 'path',                    group_index = 2 },
+      { name = 'nvim_lsp_signature_help', group_index = 3 },
     },
     formatting = {
       format = function(entry, vim_item)
