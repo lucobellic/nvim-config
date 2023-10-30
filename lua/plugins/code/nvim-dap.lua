b73a0048130aa316ddbe72bf236a97742139b408
Luco Bellic <luco.bellic@protonmail.com>
2023-10-30 23:48:08 +0100

    🔧 [nvim-dap] Add function key for debugging

--- lua/plugins/code/nvim-dap.lua
+++ lua/plugins/code/nvim-dap.lua
@@ -30,12 +30,16 @@ return {
     { '<leader>dq', require('dap').terminate, repeatable = true, desc = 'Terminate' },
     { '<leader>db', require('dap').toggle_breakpoint, repeatable = true, desc = 'Toggle Breakpoint' },
     { '<leader>dc', require('dap').continue, repeatable = true, desc = 'Continue' },
+    { '<F5>', require('dap').continue, repeatable = true, desc = 'Continue' },
     { '<leader>dC', require('dap').run_to_cursor, repeatable = true, desc = 'Run to Cursor' },
     { '<leader>di', require('dap').step_into, repeatable = true, desc = 'Step Into' },
+    { '<F11>', require('dap').step_into, repeatable = true, desc = 'Step Into' },
     { '<leader>dj', require('dap').down, repeatable = true, desc = 'Down' },
     { '<leader>dk', require('dap').up, repeatable = true, desc = 'Up' },
     { '<leader>do', require('dap').step_out, repeatable = true, desc = 'Step Out' },
+    { '<F12>', require('dap').step_out, repeatable = true, desc = 'Step Out' },
     { '<leader>dO', require('dap').step_over, repeatable = true, desc = 'Step Over' },
+    { '<F10>', require('dap').step_over, repeatable = true, desc = 'Step Over' },
     {
       '<leader>dx',
       function()
