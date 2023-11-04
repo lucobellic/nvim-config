833468746017ec1dee3f04441079ac3cde022ef4
Luco Bellic <luco.bellic@protonmail.com>
2023-11-05 00:38:17 +0100

    💄 [indent] Update indent-blankline sign

--- lua/plugins/editor/indent-blankline.lua
+++ lua/plugins/editor/indent-blankline.lua
@@ -14,8 +14,8 @@ return {
   main = 'ibl',
   opts = {
     indent = {
-      char = '▏',
-      tab_char = '▏',
+      char = '│',
+      tab_char = '│',
       highlight = 'IndentBlankLineChar',
     },
     scope = {
