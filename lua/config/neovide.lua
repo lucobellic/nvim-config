13a43cabdfc4e97dfb4c3d82aafd262798ef8d81
Luco Bellic <luco.bellic@protonmail.com>
2023-11-24 14:57:53 +0100

    🔧 [neovide] Add keymap to change font size

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,7 @@
 if vim.g.neovide then
-  vim.o.guifont = 'DMMono Nerd Font:h11'
+  local font_family = 'DMMono Nerd Font'
+  local font_size = 11
+  vim.o.guifont = font_family .. ':h' .. font_size
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_trail_size = 0.2
   vim.g.neovide_scroll_animation_length = 0
@@ -19,37 +21,55 @@ if vim.g.neovide then
   -- Colors
 
   -- black
-  vim.g.terminal_color_0  = '#0b0e14'
-  vim.g.terminal_color_8  = '#0b0e14'
+  vim.g.terminal_color_0 = '#0b0e14'
+  vim.g.terminal_color_8 = '#0b0e14'
 
   -- red
-  vim.g.terminal_color_1  = '#f26d78'
-  vim.g.terminal_color_9  = '#f26d78'
+  vim.g.terminal_color_1 = '#f26d78'
+  vim.g.terminal_color_9 = '#f26d78'
 
   -- green
-  vim.g.terminal_color_2  = '#aad94c'
+  vim.g.terminal_color_2 = '#aad94c'
   vim.g.terminal_color_10 = '#aad94c'
 
   -- yellow
-  vim.g.terminal_color_3  = '#ffb454'
+  vim.g.terminal_color_3 = '#ffb454'
   vim.g.terminal_color_11 = '#ffb454'
 
   -- blue
-  vim.g.terminal_color_4  = '#39bae6'
+  vim.g.terminal_color_4 = '#39bae6'
   vim.g.terminal_color_12 = '#39bae6'
 
   -- magenta
-  vim.g.terminal_color_5  = '#d2a6ff'
+  vim.g.terminal_color_5 = '#d2a6ff'
   vim.g.terminal_color_13 = '#d2a6ff'
 
   -- cyan
-  vim.g.terminal_color_6  = '#73b8ff'
+  vim.g.terminal_color_6 = '#73b8ff'
   vim.g.terminal_color_14 = '#73b8ff'
 
   -- white
-  vim.g.terminal_color_7  = '#bfbdb6'
+  vim.g.terminal_color_7 = '#bfbdb6'
   vim.g.terminal_color_15 = '#bfbdb6'
 
+  ---@param delta number
+  local function change_font(delta)
+    font_size = font_size + delta
+    vim.o.guifont = font_family .. ':h' .. font_size
+  end
+
+  vim.keymap.set({ 'n' }, '<C-+>', function() change_font(1) end, {
+    noremap = true,
+    silent = true,
+    repeatable = true,
+    desc = 'Increase font size',
+  })
+  vim.keymap.set({ 'n' }, '<C-->', function() change_font(-1) end, {
+    noremap = true,
+    silent = true,
+    repeatable = true,
+    desc = 'Decrease font size',
+  })
 
   vim.api.nvim_set_keymap('n', '<F11>', '<cmd>lua toggle_full_screen()<cr>', { silent = true })
 end
