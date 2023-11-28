6cc6cab6bf9312e4781c7609590b963442719fcd
Luco Bellic <luco.bellic@protonmail.com>
2023-11-28 11:58:35 +0100

    🔧 [none-ls] Replace usage of conform and nvim-lint

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,22 +1,20 @@
 local cspell_config = {
-  find_json = function(cwd)
-    return vim.fn.stdpath('config') .. '/spell/cspell.json'
-  end,
+  find_json = function(cwd) return vim.fn.stdpath('config') .. '/spell/cspell.json' end,
 }
 
 return {
   'nvimtools/none-ls.nvim',
-  enabled = false,
+  enabled = true,
   dependencies = { 'davidmh/cspell.nvim' },
   opts = function(_, opts)
     local nls = require('null-ls')
     opts.fallback_severity = vim.diagnostic.severity.INFO
     table.insert(opts.sources, nls.builtins.formatting.prettierd)
-    table.insert(opts.sources, nls.builtins.completion.spell)
-    table.insert(opts.sources, nls.builtins.formatting.codespell)
+    -- table.insert(opts.sources, nls.builtins.completion.spell)
+    -- table.insert(opts.sources, nls.builtins.formatting.codespell)
 
     local cspell = require('cspell')
-    table.insert(opts.sources, cspell.diagnostics.with({ config = cspell_config }))
-    table.insert(opts.sources, cspell.code_actions.with({ config = cspell_config }))
+    table.insert(opts.sources, cspell.diagnostics.with({ filetypes = {}, config = cspell_config }))
+    table.insert(opts.sources, cspell.code_actions.with({ filetypes = {}, config = cspell_config }))
   end,
 }
--- lua/plugins/lsp/nvim-lint.lua
+++ lua/plugins/lsp/nvim-lint.lua
@@ -1,19 +1,22 @@
 return {
   'mfussenegger/nvim-lint',
+  enabled = false,
   opts = function(_, opts)
-    -- Configure cspell to using config file
+    -- Configure cspell using config file
     local cspell_args = require('lint.linters.cspell').args
     local cspell_config_file = vim.fn.stdpath('config') .. '/spell/cspell.json'
     require('lint.linters.cspell').args = vim.tbl_extend('force', cspell_args, {
       '--config',
       cspell_config_file,
     })
-    -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-    -- 	pattern = "*",
-    -- 	callback = function(ev)
-    -- 		require("lint").try_lint({ "cspell" })
-    -- 	end,
-    -- 	desc = "Perform cspell lint for all files",
-    -- })
+
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter', 'InsertLeave' }, {
+      group = vim.api.nvim_create_augroup('lint', { clear = true }),
+      pattern = '*',
+      callback = function()
+        require('lint').try_lint('cspell')
+      end,
+      desc = 'Lint cspell',
+    })
   end,
 }
