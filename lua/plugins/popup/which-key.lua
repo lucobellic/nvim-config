af43c8f81de0f11b1e745daf2755632b90b274b6
Luco Bellic <luco.bellic@protonmail.com>
2023-12-09 18:10:24 +0100

    💄 [which-key] Remove group prefix

--- lua/plugins/popup/which-key.lua
+++ lua/plugins/popup/which-key.lua
@@ -20,7 +20,7 @@ return {
     icons = {
       breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
       separator = '➜', -- symbol used between a key and it's label
-      group = '⁘ ', -- symbol prepended to a group
+      group = '', -- symbol prepended to a group
     },
     window = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
