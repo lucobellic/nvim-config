68c255e0acd34ac825ef83315df71ff90c55d21b
Luco Bellic <luco.bellic@protonmail.com>
2023-12-15 16:37:09 +0100

    💄 [neoscroll] Disable if neovide is in use

--- lua/plugins/ui/neoscroll.lua
+++ lua/plugins/ui/neoscroll.lua
@@ -1,7 +1,6 @@
 return {
-  "karb94/neoscroll.nvim",
-  event="VeryLazy",
-  config = function ()
-    require('neoscroll').setup {}
-  end
+  'karb94/neoscroll.nvim',
+  event = 'VeryLazy',
+  enabled = not vim.g.neovide,
+  config = function() require('neoscroll').setup({}) end,
 }
