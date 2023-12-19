ca1ad08b4e14c1609dc8fcfa6b5160c26e571925
Luco Bellic <luco.bellic@protonmail.com>
2023-12-19 22:03:02 +0100

    🩹 [commands] correct usage of title case

--- lua/util/commands.lua
+++ lua/util/commands.lua
@@ -4,29 +4,36 @@ local M = {}
 ---@param str string
 ---@return string
 local function command_title_case(str)
-  -- Replace special charactes with spaces and convert to title case
+  -- Replace special character with spaces and convert to title case
   ---@diagnostic disable-next-line
   return str
     :gsub('%W', ' ')
-    :gsub("(%a)([%w_']*)", function(first, rest)
-      return first:upper() .. rest:lower()
-    end)
+    :gsub("(%a)([%w_']*)", function(first, rest) return first:upper() .. rest end)
     :gsub('%s+', '')
 end
 
---- Create command from keymaps
---- Iterate over all keymaps and create command for description
---- only if command does not already exist
+---Create user command from keymap
+---@param command_name string
+---@param keymap table<string, any>
+local function create_command_from_keymap(command_name, keymap)
+  local cmd = vim.api.nvim_replace_termcodes(keymap.lhs, true, false, true)
+  vim.api.nvim_create_user_command(
+    command_name,
+    function() vim.api.nvim_feedkeys(cmd, 't', true) end,
+    { desc = keymap.desc }
+  )
+end
+
+---Create command from keymaps
+---Iterate over all keymaps and create command for description
+---only if command does not already exist
 function M.create_command_from_keymaps()
   local keymaps = vim.api.nvim_get_keymap('n')
-  for _, value in ipairs(keymaps or {}) do
-    if value.desc then
-      local command_name = command_title_case(value.desc)
-      if vim.fn.exists(':' .. command_name) == 0 then
-        vim.api.nvim_create_user_command(command_name, function()
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(value.lhs, true, false, true), 't', true)
-        end, { desc = value.desc })
-      end
+  local keymaps_with_desc = vim.tbl_filter(function(keymap) return keymap.desc ~= nil end, keymaps)
+  for _, keymap in ipairs(keymaps_with_desc or {}) do
+    local command_name = command_title_case(keymap.desc)
+    if vim.fn.exists(':' .. command_name) == 0 then
+      create_command_from_keymap(command_name, keymap)
     end
   end
 end
