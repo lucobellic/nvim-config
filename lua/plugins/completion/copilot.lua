9f41c4965dc9af49f022d1be5ed7fe4d5dc30fb7
Luco Bellic <luco.bellic@protonmail.com>
2024-01-05 22:01:39 +0100

    🔧 (copilot): enable for multiple file types

---
+++ lua/plugins/completion/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  'zbirenbaum/copilot.lua',
+  opts = {
+    filetypes = {
+      markdown = true,
+      gitcommit = true,
+      gitrebase = true,
+      hgcommit = true,
+      svn = true,
+    },
+  },
+}
