fa4b6333ade7349c760daee26e99976654c11cd3
Luco Bellic <luco.bellic@protonmail.com>
2024-01-05 22:03:11 +0100

    🩹 (nvim-cmp): fix group index

--- lua/plugins/completion/nvim-cmp.lua
+++ lua/plugins/completion/nvim-cmp.lua
@@ -140,8 +140,8 @@ return {
     mapping = cmp.mapping.preset.insert(tab_confirm_mapping),
     -- vim.tbl_extend('force', opts.sources, {
     sources = {
-      { name = 'copilot', group_index = 1 },
-      { name = 'nvim_lsp', group_index = 1 },
+      { name = 'copilot', group_index = 2 },
+      { name = 'nvim_lsp', group_index = 2 },
       { name = 'luasnip', group_index = 2 },
       { name = 'buffer', group_index = 2 },
       { name = 'path', group_index = 2 },
