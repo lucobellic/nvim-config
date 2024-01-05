6bea9a614926540cc87c833db984d49df0422283
Luco Bellic <luco.bellic@protonmail.com>
2024-01-05 23:08:01 +0100

    💄 (neovide): update transparency and shadows

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -4,10 +4,11 @@ if vim.g.neovide then
   vim.o.guifont = font_family .. ':h' .. font_size
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_trail_size = 0.2
-  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0.2
+  vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.g.neovide_transparency = 1.0
-  vim.g.neovide_transparency_point = 1.0
+  -- vim.g.neovide_transparency = 0.85
+  -- vim.g.neovide_transparency_point = 1.0
 
   -- Disable mini.animate with neovide
   vim.g.minianimate_disable = true
@@ -49,6 +50,12 @@ if vim.g.neovide then
   -- white
   vim.g.terminal_color_7 = '#bfbdb6'
   vim.g.terminal_color_15 = '#bfbdb6'
+
+  -- Shadow
+  vim.g.neovide_floating_shadow = false
+  vim.g.neovide_floating_z_height = 20
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
 
   ---@param delta number
   local function change_font(delta)
