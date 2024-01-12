bc90b2e0bc8d864d2441aff549612125b0a91469
Luco Bellic <luco.bellic@protonmail.com>
2024-01-12 23:00:53 +0100

    💄 (zenmode): disable statusline in zenmode

--- lua/plugins/editor/zenmode.lua
+++ lua/plugins/editor/zenmode.lua
@@ -42,6 +42,7 @@ return {
           enabled = true,
           ruler = false, -- disables the ruler text in the cmd line area
           showcmd = false, -- disables the command in the last line of the screen
+          laststatus = 0, -- turn off the statusline in zen mode
         },
         incline = { enabled = false }, -- disables the incline markers
         gitsigns = { enabled = false }, -- disables git signs
