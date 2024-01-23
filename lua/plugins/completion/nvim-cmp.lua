c6607d98934ad8685518cfcf147106fddf7f3973
Luco Bellic <luco.bellic@protonmail.com>
2024-01-23 22:34:36 +0100

    ⚰️  (nvim-cmp): remove dead code

--- lua/plugins/completion/nvim-cmp.lua
+++ lua/plugins/completion/nvim-cmp.lua
@@ -73,36 +73,6 @@ local tab_confirm_mapping = {
   end, { 'i', 'c' }),
 }
 
--- Use tab to select an item and esc to insert it
--- Escaping during navigation with up/down aborts the completion
-local tab_selection_mapping = {
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-  ['<cr>'] = safely_select,
-
-  ['<Tab>'] = cmp.mapping(function(fallback)
-    if cmp.visible() and has_words_before() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
-
-  ['<S-Tab>'] = cmp.mapping(function(fallback)
-    if cmp.visible() and has_words_before() then
-      cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    else
-      fallback()
-    end
-  end, { 'i', 's' }),
-}
-
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
