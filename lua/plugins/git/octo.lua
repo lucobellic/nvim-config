f9e9c6c519f29641a38d307ea55d151d654a7f33
Luco Bellic <luco.bellic@protonmail.com>
2024-01-24 23:23:20 +0100

    🩹 (octo): fix dependencies

--- lua/plugins/git/octo.lua
+++ lua/plugins/git/octo.lua
@@ -1,7 +1,7 @@
 return {
   'pwntester/octo.nvim',
   event = 'VeryLazy',
-  requires = {
+  dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
     'nvim-tree/nvim-web-devicons',
