169f0ddab6a1b3275eb6084fc5f3c9e2a8d6c3d5
Luco Bellic <luco.bellic@protonmail.com>
2024-02-03 20:25:24 +0100

    🔧 (nvim-ufo): improve zm/zr fold usage

--- lua/plugins/editor/nvim-ufo.lua
+++ lua/plugins/editor/nvim-ufo.lua
@@ -1,42 +1,41 @@
+-- return the max fold level of the buffer (for now doing the opposite and folding incrementally is unbounded)
+-- Also jarring if you start folding incrementally after opening all folds
+local function max_level()
+  -- return vim.wo.foldlevel -- find a way for this to return max fold level
+  return 0
+end
+
+---Set the fold level to the provided value and store it locally to the buffer
+---@param num integer the fold level to set
+local function set_fold(num)
+  -- vim.w.ufo_foldlevel = math.min(math.max(0, num), max_level()) -- when max_level is implemneted properly
+  vim.b.ufo_foldlevel = math.max(0, num)
+  require('ufo').closeFoldsWith(vim.b.ufo_foldlevel)
+end
+
+--Shift the current fold level by the provided amount
+---@param dir number positive or negative number to add to the current fold level to shift it
+local shift_fold = function(dir) set_fold((vim.b.ufo_foldlevel or max_level()) + dir) end
+
+-- when max_level is implemented properly
+-- vim.keymap.set("n", "zR", function() set_win_fold(max_level()) end, { desc = "Open all folds" })
+
 return {
   'kevinhwang91/nvim-ufo',
   event = 'VeryLazy',
-  dependencies = {
-    'kevinhwang91/promise-async',
-    {
-      'luukvbaal/statuscol.nvim',
-      enabled = false,
-      opts = {
-        ft_ignore = {
-          'Outline',
-          'sagaoutline',
-        },
-      },
-      config = function()
-        local builtin = require('statuscol.builtin')
-        require('statuscol').setup({
-          relculright = true,
-          segments = {
-            { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
-            { text = { '%s' }, click = 'v:lua.ScSa' },
-            { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
-          },
-        })
-      end,
-    },
-  },
+  dependencies = { 'kevinhwang91/promise-async' },
   keys = {
+    { 'zR', function() require('ufo').openAllFolds() end, { desc = 'Open all folds' } },
+    { 'zM', function() set_fold(0) end, { desc = 'Close all folds' } },
     {
-      'zR',
-      function()
-        require('ufo').openAllFolds()
-      end,
+      'zr',
+      function() shift_fold(vim.v.count == 0 and 1 or vim.v.count) end,
+      { repeatable = true, desc = 'Fold less' },
     },
     {
-      'zM',
-      function()
-        require('ufo').closeAllFolds()
-      end,
+      'zm',
+      function() shift_fold(-(vim.v.count == 0 and 1 or vim.v.count)) end,
+      { repeatable = true, desc = 'Fold more' },
     },
   },
   opts = {},
