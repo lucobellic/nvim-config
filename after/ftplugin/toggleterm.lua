6d4c0f0ff0e709376c58b63350244ae23c8237c5
Luco Bellic <luco.bellic@protonmail.com>
2024-02-03 19:07:30 +0100

    ♻️  (toggleterm): update term close and focus

--- after/ftplugin/toggleterm.lua
+++ after/ftplugin/toggleterm.lua
@@ -3,3 +3,23 @@ vim.b.minianimate_disable = true
 vim.b.miniindentscope_disable = true
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
+
+local function term_focus_offset(offset)
+  local terminal = require('toggleterm.terminal')
+  local current_id = terminal.get_focused_id()
+  local term = terminal.get(current_id + offset)
+  if term then
+    if term:is_open() then
+      term:focus()
+    else
+      term:open()
+    end
+  end
+end
+
+local opts = { buffer = 0 }
+vim.keymap.set({ 't' }, '<esc>', [[<C-\><C-n>]], opts)
+vim.keymap.set({ 't', 'n' }, '<C-h>', function() term_focus_offset(-1) end, opts)
+vim.keymap.set({ 't', 'n' }, '<C-l>', function() term_focus_offset(1) end, opts)
+vim.keymap.set({ 't', 'n' }, '<C-q>', '<cmd>ToggleTermShutdown<cr>', opts)
+vim.keymap.set({ 't', 'n' }, '<C-t>', '<cmd>ToggleTermSplit<cr>', opts)
--- lua/plugins/popup/toggleterm.lua
+++ lua/plugins/popup/toggleterm.lua
@@ -4,39 +4,40 @@ local function split_terminal_right()
 end
 vim.api.nvim_create_user_command('ToggleTermSplit', split_terminal_right, {})
 
-local function shutdown_currrent_term()
+-- Focus the closest terminal before the current one or next one otherwise
+---@param term_id number id of the terminal to focus
+local function focus_closest_prev_term(term_id)
+  local terminal = require('toggleterm.terminal')
+  local open_terms = vim.tbl_filter(function(term) return term:is_open() end, terminal.get_all(false))
+  table.sort(open_terms, function(a, b) return a.id < b.id end)
+
+  -- Find the current terminal by id in the sorted list
+  local id = 0
+  for i, term in ipairs(open_terms) do
+    if term.id == term_id then
+      id = i
+      break
+    end
+  end
+
+  -- Focus to the closest terminal previous first or next one
+  local closest_term = open_terms[id - 1] or open_terms[id + 1]
+  if closest_term then
+    closest_term:focus()
+  end
+end
+
+-- Close the current terminal and focus to the closest previous terminal if available
+local function shutdown_current_term()
   local terminal = require('toggleterm.terminal')
   local term = terminal.get(terminal.get_focused_id())
   if term then
+    focus_closest_prev_term(term.id)
     term:shutdown()
   end
 end
-vim.api.nvim_create_user_command('ToggleTermShutdown', shutdown_currrent_term, {})
 
-local function term_focus_offset(offset)
-  local terminal = require('toggleterm.terminal')
-  local current_id = terminal.get_focused_id()
-  local term = terminal.get(current_id + offset)
-  if term then
-    if term:is_open() then
-      term:focus()
-    else
-      term:open()
-    end
-  end
-end
-
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set({ 't' }, '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set({ 't', 'n' }, '<C-h>', function() term_focus_offset(-1) end, opts)
-  vim.keymap.set({ 't', 'n' }, '<C-l>', function() term_focus_offset(1) end, opts)
-  vim.keymap.set({ 't', 'n' }, '<C-q>', '<cmd>ToggleTermShutdown<cr>', opts)
-  vim.keymap.set({ 't', 'n' }, '<C-t>', '<cmd>ToggleTermSplit<cr>', opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+vim.api.nvim_create_user_command('ToggleTermShutdown', shutdown_current_term, {})
 
 return {
   'akinsho/toggleterm.nvim',
