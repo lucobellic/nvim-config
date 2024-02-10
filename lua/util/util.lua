ac5734e169bf8c0e7a126a2bce1d959e61f78570
Luco Bellic <luco.bellic@protonmail.com>
2024-02-10 15:06:56 +0100

    🩹 (gp.nvim) update file detection regex

--- lua/util/util.lua
+++ lua/util/util.lua
@@ -1,5 +1,9 @@
 local M = {}
 -- Check if a file is a gp.nvim file type
 ---@param file_name string?
-function M.is_gp_file(file_name) return file_name and file_name:match('%d*-%d*-%d*.%d*-%d*-%d*.%d*') end
+function M.is_gp_file(file_name)
+  local regex = '%d*-%d*-%d*.%d*-%d*-%d*.%d*.md'
+  return file_name and file_name:match(regex)
+end
+
 return M
