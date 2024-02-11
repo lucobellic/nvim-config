912ee7243f4cfa64ac40e49bcf0e5c6539b7f715
Luco Bellic <luco.bellic@protonmail.com>
2024-02-11 15:43:22 +0100

    ➕ (plugins): add nvim-window plugin

---
+++ lua/plugins/navigation/nvim-window.lua
@@ -0,0 +1,50 @@
+return {
+  'yorickpeterse/nvim-window',
+  keys = {
+    { '<leader>k', "<cmd>lua require('nvim-window').pick()<cr>", desc = 'Jump To Window' },
+  },
+  opts = {
+    -- A group to use for overwriting the Normal highlight group in the floating
+    -- window. This can be used to change the background color.
+    normal_hl = 'Normal',
+
+    -- The highlight group to apply to the line that contains the hint characters.
+    -- This is used to make them stand out more.
+    hint_hl = 'FloatTitle',
+
+    -- The border style to use for the floating window.
+    border = 'none',
+
+    -- The characters available for hinting windows.
+    chars = {
+      'a',
+      's',
+      'd',
+      'f',
+      'g',
+      'h',
+      'j',
+      'k',
+      'l',
+      ';',
+      'q',
+      'w',
+      'e',
+      'r',
+      't',
+      'y',
+      'u',
+      'i',
+      'o',
+      'p',
+      'z',
+      'x',
+      'c',
+      'v',
+      'b',
+      'n',
+      'm',
+      ',',
+    },
+  },
+}
