d6486f1964efe73affe24c428c1f009cfd46d98a
Luco Bellic <luco.bellic@protonmail.com>
2024-02-17 19:04:25 +0100

    💄 (incline): add filename with icons

--- lua/plugins/ui/incline.lua
+++ lua/plugins/ui/incline.lua
@@ -33,19 +33,32 @@ local function get_git_diff(props)
 end
 
 local function get_toggleterm_id(props)
-  local id = vim.fn.bufname(props.buf):sub(-1)
-  return { { id, group = props.focused and 'Title' or 'FloatBorder' } }
+  local id = ' ' .. vim.fn.bufname(props.buf):sub(-1) .. ' '
+  return { { id, group = props.focused and 'FloatTitle' or 'Title' } }
 end
 
 local function is_toggleterm(bufnr) return vim.bo[bufnr].filetype == 'toggleterm' end
 
-local edgy_filetypes = { 'neotest-panel', 'noice', 'Trouble', 'OverseerList', 'Outline' }
-local edgy_titles = { ['neotest-panel'] = 'neotest', noice = 'noice', Trouble = 'trouble', OverseerList = 'overseer', Outline = 'outline' }
+local edgy_filetypes = {
+  'neotest-panel',
+  'noice',
+  'Trouble',
+  'OverseerList',
+  'Outline',
+}
+
+local edgy_titles = {
+  ['neotest-panel'] = 'neotest',
+  noice = 'noice',
+  Trouble = 'trouble',
+  OverseerList = 'overseer',
+  Outline = 'outline',
+}
 local function is_edgy_group(props) return vim.tbl_contains(edgy_filetypes, vim.bo[props.buf].filetype) end
 
 local function get_title(props)
-  local title = edgy_titles[vim.bo[props.buf].filetype]
-  return { { title, group = props.focused and 'Title' or 'FloatBorder' } }
+  local title = ' ' .. edgy_titles[vim.bo[props.buf].filetype] .. ' '
+  return { { title, group = props.focused and 'FloatTitle' or 'Title' } }
 end
 
 return {
@@ -82,10 +95,19 @@ return {
         return get_title(props)
       end
 
+      local filename = vim.fn.fnamemodify(vim.fn.bufname(props.buf), ':t')
+      local filetype_icon, color = require('nvim-web-devicons').get_icon_color(filename)
       local diagnostics = get_diagnostic_label(props)
       local diffs = get_git_diff(props)
-      local separator = (#diagnostics > 0 and #diffs > 0) and { separator_char .. ' ', group = 'NonText' } or ''
+
+      local separator = (#diagnostics > 0 and #diffs > 0) and { separator_char .. ' ', group = 'FloatBorder' } or ''
+      local icon = props.focused and { filetype_icon, guifg = color } or { filetype_icon, group = 'Directory' }
+      local filename_separator = (#diagnostics > 0 or #diffs > 0) and { ' --- ', group = 'FloatBorder' } or ''
       local buffer = {
+        icon,
+        { ' ' },
+        { filename, group = props.focused and 'Identifier' or 'Directory' },
+        filename_separator,
         { diagnostics },
         { separator },
         { diffs },
