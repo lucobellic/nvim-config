95b87d49c09bd40b9793c3a5ff193a312318421f
Luco Bellic <luco.bellic@protonmail.com>
2024-03-08 17:24:36 +0100

    🔧 (neotest): update neotest to use opts

--- lua/plugins/code/neotest.lua
+++ lua/plugins/code/neotest.lua
@@ -4,52 +4,47 @@ return {
     'nvim-neotest/neotest-plenary',
     'nvim-neotest/neotest-vim-test',
     'nvim-neotest/neotest-python',
-    { 'rouge8/neotest-rust', enabled = false }, -- disable neotest-rust in favor of rustaceanvim
-    'mrcjkb/rustaceanvim',
   },
   cmd = { 'Neotest' },
-  opts = {
-    adapters = {},
-    status = {
-      enabled = true,
-      virtual_text = true,
-      signs = false,
-    },
-    output = {
-      enabled = true,
-      open_on_run = false,
-    },
-    icons = {
-      running_animated = { '', '', '', '' },
-      passed = '',
-      running = '',
-      failed = '',
-      skipped = '',
-      unknown = '',
-      non_collapsible = '─',
-      collapsed = '─',
-      expanded = '╮',
-      child_prefix = '├',
-      final_child_prefix = '╰',
-      child_indent = '│',
-      final_child_indent = ' ',
-      watching = '',
-    },
-  },
-  config = function(_, opts)
-    opts.adapters = {
-      require('neotest-plenary'),
-      require('neotest-vim-test'),
-      require('neotest-python')({
-        runner = 'pytest',
-        python = '/usr/bin/python',
-        pytest_discover_instances = true, -- experimental
-      }),
-      require('rustaceanvim.neotest'),
-    }
-    opts.consumers = {
-      overseer = require('neotest.consumers.overseer'),
-    }
-    require('neotest').setup(opts)
+  opts = function(_, opts)
+    -- Configure neotest-python
+    opts.adapters = opts.adapters or {}
+    opts.adapters['neotest-python'] = vim.tbl_deep_extend('force', opts.adapters['neotest-python'] or {}, {
+      runner = 'pytest',
+      python = '/usr/bin/python',
+      pytest_discover_instances = true, -- experimental
+    })
+
+    opts = vim.tbl_deep_extend('force', opts, {
+      status = {
+        enabled = true,
+        virtual_text = true,
+        signs = false,
+      },
+      output = {
+        enabled = true,
+        open_on_run = false,
+      },
+      icons = {
+        running_animated = { '', '', '', '' },
+        passed = '',
+        running = '',
+        failed = '',
+        skipped = '',
+        unknown = '',
+        non_collapsible = '─',
+        collapsed = '─',
+        expanded = '╮',
+        child_prefix = '├',
+        final_child_prefix = '╰',
+        child_indent = '│',
+        final_child_indent = ' ',
+        watching = '',
+      },
+      consumers = {
+        overseer = require('neotest.consumers.overseer'),
+      },
+    })
+    return opts
   end,
 }
