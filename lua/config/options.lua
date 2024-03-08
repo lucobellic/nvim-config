7d226d21bae7d6e4e2f2f78b9671c8e719ac62ca
Luco Bellic <luco.bellic@protonmail.com>
2024-03-08 17:23:55 +0100

    ⚰️  (options): remove unused custom fold function

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -57,12 +57,6 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-function _G.custom_fold_text()
-  local line = vim.fn.getline(vim.v.foldstart)
-  local line_count = vim.v.foldend - vim.v.foldstart + 1
-  return line .. ' ... ' .. line_count
-end
-
 vim.o.foldtext = 'v:lua.custom_fold_text()'
 vim.o.virtualedit = 'block'
 
