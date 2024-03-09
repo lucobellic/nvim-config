990bcc205f4a5f93d9b520cc87d472fc42cdebc2
Luco Bellic <luco.bellic@protonmail.com>
2024-03-09 19:47:51 +0100

    🎨 (persistence): format code

--- lua/plugins/popup/persistence.lua
+++ lua/plugins/popup/persistence.lua
@@ -4,30 +4,22 @@ return {
   keys = {
     {
       '<leader>qr',
-      function()
-        require('persistence').load()
-      end,
+      function() require('persistence').load() end,
       desc = 'Restore session',
     },
     {
       '<leader>ql',
-      function()
-        require('persistence').load({ last = true })
-      end,
+      function() require('persistence').load({ last = true }) end,
       desc = 'Load last session',
     },
     {
       '<leader>qd',
-      function()
-        require('persistence').stop()
-      end,
+      function() require('persistence').stop() end,
       desc = 'Stop session',
     },
     {
       '<leader>qs',
-      function()
-        require('persistence').save()
-      end,
+      function() require('persistence').save() end,
       desc = 'Save session',
     },
   },
