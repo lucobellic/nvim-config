c30a0ccb0e383aef170bd89a2ebdfc1381852897
Luco Bellic <luco.bellic@protonmail.com>
2024-03-12 18:14:55 +0100

    🔧 (toggleterm): add toggleterm navigation keymaps

--- after/ftplugin/toggleterm.lua
+++ after/ftplugin/toggleterm.lua
@@ -7,17 +7,21 @@ vim.opt_local.relativenumber = false
 local function term_focus_offset(offset)
   local terminal = require('toggleterm.terminal')
   local current_id = terminal.get_focused_id()
-  local term = terminal.get(current_id + offset)
-  if term then
-    if term:is_open() then
-      term:focus()
-    else
-      term:open()
+  if current_id then
+    local term = terminal.get(current_id + offset)
+    if term then
+      if term:is_open() then
+        term:focus()
+      else
+        term:open()
+      end
     end
   end
 end
 
 local opts = { buffer = 0 }
 vim.keymap.set({ 't' }, '<esc>', [[<C-\><C-n>]], opts)
+vim.keymap.set({ 't', 'n' }, '<S-h>', function() term_focus_offset(-1) end, opts)
+vim.keymap.set({ 't', 'n' }, '<S-l>', function() term_focus_offset(1) end, opts)
 vim.keymap.set({ 't', 'n' }, '<C-q>', '<cmd>ToggleTermShutdown<cr>', opts)
 vim.keymap.set({ 't', 'n' }, '<C-t>', '<cmd>ToggleTermSplit<cr>', opts)
