ecedc1a3520a4556cc3905daa7aa4e3a604f7c98
Luco Bellic <luco.bellic@protonmail.com>
2024-03-30 15:43:01 +0100

    🔧 (spectre): update rendering and add options

--- lua/plugins/editor/nvim-spectre.lua
+++ lua/plugins/editor/nvim-spectre.lua
@@ -1,11 +1,53 @@
 return {
   'nvim-pack/nvim-spectre',
   opts = {
+    line_sep_start = '╭─────────────────────────────────────────',
+    result_padding = '│  ',
+    line_sep       = '╰─────────────────────────────────────────',
+    mapping = {
+      ['toggle_multiline'] = {
+        map = 'tm',
+        cmd = "<cmd>lua require('spectre').change_options('multiline')<CR>",
+        desc = 'toggle search multiline',
+      },
+    },
+    find_engine = {
+      -- rg is map with finder_cmd
+      ['rg'] = {
+        cmd = 'rg',
+        -- default args
+        args = {
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--smart-case',
+          '--line-number',
+          '--column',
+          '-j4',
+        },
+        options = {
+          ['ignore-case'] = {
+            value = '--ignore-case',
+            icon = '[I]',
+            desc = 'ignore case',
+          },
+          ['hidden'] = {
+            value = '--hidden',
+            desc = 'hidden file',
+            icon = '[H]',
+          },
+          ['multiline'] = {
+            value = '--multiline',
+            desc = 'multiline',
+            icon = '[M]',
+          },
+        },
+      },
+    },
     highlight = {
       ui = 'Title',
       search = 'Search',
       replace = 'Substitute',
     },
-    result_padding = '▏  ',
   },
 }
