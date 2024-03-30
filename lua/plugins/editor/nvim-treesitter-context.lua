9824635f60aaa518b2436ddbb6c700fb0d35f7ed
Luco Bellic <luco.bellic@protonmail.com>
2024-03-30 17:06:28 +0100

    💄 (treesitter-context): update context rendering

--- lua/plugins/editor/nvim-treesitter-context.lua
+++ lua/plugins/editor/nvim-treesitter-context.lua
@@ -1,7 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
   opts = {
+    max_lines = 3,
     line_numbers = true,
-    separator = '·',
+    trim_scope = 'inner',
+    separator = vim.g.neovide and ' ' or '·',
   },
 }
