4421b80f232c40cc5f193237d84371e9525dea32
Luco Bellic <luco.bellic@protonmail.com>
2024-04-01 23:43:37 +0200

    ✨ (diffivew): add commit fixup

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -22,7 +22,10 @@ return {
       'DiffviewRangeFileHistory',
       'DiffviewOriginDevelopHead',
     },
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neotest/nvim-nio',
+    },
     keys = {
       {
         '<leader>gdg',
@@ -100,6 +103,39 @@ return {
               '<C-g>',
               require('diffview.actions').open_in_diffview,
               { desc = 'Open the entry under the cursor in a diffview' },
+            },
+            {
+              'n',
+              '<C-s>',
+              function()
+                local lazy = require('diffview.lazy')
+                ---@type FileHistoryView|LazyModule
+                local FileHistoryView =
+                  lazy.access('diffview.scene.views.file_history.file_history_view', 'FileHistoryView')
+                local view = require('diffview.lib').get_current_view()
+                if view and view:instanceof(FileHistoryView.__get()) then
+                  ---@cast view DiffView|FileHistoryView
+                  local file = view:infer_cur_file()
+                  local item = view.panel:get_item_at_cursor()
+
+                  if file and item then
+                    local nio = require('nio')
+                    local path = file.absolute_path
+                    nio.run(function()
+                      nio.process.run({ cmd = 'git', args = { 'stash', '--keep-index' } }).result()
+                      nio.process
+                        .run({
+                          cmd = 'git',
+                          args = { 'commit', '--fixup=' .. item.commit.hash, '--', path },
+                        })
+                        .result()
+                      nio.process.run({ cmd = 'git', args = { 'stash', 'pop', '--index' } }).result()
+                      vim.notify('Fixup ' .. item.commit.hash, vim.log.levels.INFO)
+                    end)
+                  end
+                end
+              end,
+              { desc = 'Fixup current file staged change' },
             },
           },
         },
