ea96c3cb276b83fd36d3d5efda77df85b60b7391
Luco Bellic <luco.bellic@protonmail.com>
2024-05-11 14:26:11 +0200

    🩹 (shell): correct powershell option

--- lua/config/shell.lua
+++ lua/config/shell.lua
@@ -4,6 +4,9 @@ if vim.fn.executable('zsh') == 1 then
   vim.o.shellcmdflag='-c'
 elseif vim.fn.executable('pwsh') == 1 then
   vim.o.shell = 'pwsh'
+  vim.o.shellcmdflag='-c'
+  vim.o.shellquote='"'
+  vim.o.shellxquote=''
 elseif vim.fn.executable('bash') == 1 then
   vim.o.shell = 'bash'
 else
