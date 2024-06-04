e903e19908790e76355f1077b6cc8d6d4a5462f6
Luco Bellic <luco.bellic@protonmail.com>
2024-06-04 22:14:21 +0200

    💄 (copilot-chat): update separator

--- lua/plugins/code/copilot-chat.lua
+++ lua/plugins/code/copilot-chat.lua
@@ -21,7 +21,7 @@ return {
       question_header = '``` ```', -- Header to use for user questions
       answer_header = '``` ```', -- Header to use for AI answers
       error_header = '``` ```', -- Header to use for errors
-      separator = ':', -- Separator to use in chat
+      separator = '', -- Separator to use in chat
       window = {
         layout = 'vertical',
       },
