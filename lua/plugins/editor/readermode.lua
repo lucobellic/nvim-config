b43a2e143472303e7f196f0a60a3b0309b613cf4
Luco Bellic <luco.bellic@protonmail.com>
2024-06-24 16:36:02 +0200

    ➕ (plugins): add readermode plugin

---
+++ lua/plugins/editor/readermode.lua
@@ -0,0 +1,11 @@
+return {
+  'sarrisv/readermode.nvim',
+  opts = {
+    enabled = false,
+    keymap = "<leader>zc"
+  },
+  keys = {
+
+    { '<leader>zc', '<cmd>ReaderMode<cr>', desc = 'Toggle ReaderMode' },
+  },
+}
