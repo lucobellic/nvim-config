d96bc51ef6a41fc9dd85c9502e0f65957625bbac
Luco Bellic <luco.bellic@protonmail.com>
2024-06-24 16:35:39 +0200

    🔧 (neogen): remove duplicated keymaps

--- lua/plugins/lsp/neogen.lua
+++ lua/plugins/lsp/neogen.lua
@@ -1,25 +1,6 @@
 return {
   'danymat/neogen',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
-  keys = {
-    {
-      '<leader>nf',
-      function()
-        require('neogen').generate({})
-      end,
-      desc = 'Document',
-    },
-    {
-      '<leader>nc',
-      function()
-        require('neogen').generate({ type = 'class' })
-      end,
-      desc = 'Document class',
-    },
-  },
   opts = {
-    snippet_engine = 'luasnip',
-    input_after_comment = false,
     languages = {
       python = {
         template = {
