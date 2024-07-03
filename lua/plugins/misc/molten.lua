b8c5aea32488021d3840d0b6919e5b87c2ab4d98
Luco Bellic <luco.bellic@protonmail.com>
2024-07-03 13:21:46 +0200

    🔧 (molten): update keymap and pypercent usage

--- lua/plugins/misc/molten.lua
+++ lua/plugins/misc/molten.lua
@@ -27,7 +27,7 @@ local dependencies = is_kitty and kitty_dependencies or is_wezterm and wezterm_d
 local image_provider = is_kitty and 'image.nvim' or is_wezterm and 'wezterm' or ''
 
 local function evaluate_pypercent()
-  local pattern = '#%%'
+  local pattern = '%%'
   -- Find first and last line number containing #%%
   -- flags:
   --  - W : no wrap around
@@ -48,18 +48,18 @@ return {
     keys = {
       { '<leader>mi', '<cmd>MoltenInit<CR>', desc = 'Molten Init' },
       { '<leader>me', '<cmd>MoltenEvaluateOperator<CR>', desc = 'Molten Evaluate Operator' },
-      { '<leader>ml', '<cmd>MoltenEvaluateLine<CR>', desc = 'Molten Evaluate Line' },
       { '<leader>mc', '<cmd>MoltenReevaluateCell<CR>', desc = 'Molten Reevaluate Cell' },
       { '<leader>md', '<cmd>MoltenDelete<CR>', desc = 'Molten Delete' },
       { '<leader>mh', '<cmd>MoltenHideOutput<CR>', desc = 'Molten Hide Output' },
-      { '<leader>mo', '<cmd>MoltenEnterOutput<CR>', desc = 'Molten Enter Output' },
+      { '<leader>mo', ':noautocmd MoltenEnterOutput<CR>', desc = 'Molten Enter Output' },
       {
         '<leader>ms',
         function() evaluate_pypercent() end,
         desc = 'Molten Percent',
       },
+      { '<leader>ml', '<cmd>MoltenEvaluateLine<CR>', desc = 'Molten Evaluate Line' },
       {
-        '<leader>mv',
+        '<leader>ml',
         ':<C-U>MoltenEvaluateVisual<CR>gv',
         mode = 'v',
         desc = 'Molten Evaluate Visual',
