feada0d296165530f0bf508f98cabf9cc21cd0e3
Luco Bellic <luco.bellic@protonmail.com>
2024-07-06 11:17:35 +0200

    🔧 (markview): configure and set as disable
    markview is not working properly for new

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,4 +1,20 @@
 return {
-  'lukas-reineke/headlines.nvim',
-  enabled = false,
+  {
+    'lukas-reineke/headlines.nvim',
+    enabled = false,
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    enabled = false, -- Too buggy and slow to be usable
+    opts = {
+      -- Do not work
+      -- headings = {
+      --   shift_width = 0,
+      -- },
+      checkboxes = {
+        checked = { text = '󱍧', hl = 'DiagnosticOk' },
+        unchecked = { text = '󱍫', hl = 'DiagnosticInfo' },
+      },
+    },
+  },
 }
