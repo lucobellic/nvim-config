e83929501b4bfa1fbccb7c3fdcfb7da8fc6df873
Luco Bellic <luco.bellic@protonmail.com>
2024-07-06 11:17:09 +0200

    🩹 (firenvim): update installation and startup

--- lua/plugins/misc/firenvim.lua
+++ lua/plugins/misc/firenvim.lua
@@ -2,8 +2,12 @@ return {
   'glacambre/firenvim',
   -- Lazy load firenvim
   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-  lazy = not vim.g.started_by_firenvim,
-  build = function() vim.fn['firenvim#install'](0) end,
+  lazy = false,
+  cond = not not vim.g.started_by_firenvim,
+  build = function()
+    require('lazy').load({ plugins = 'firenvim', wait = true })
+    vim.fn['firenvim#install'](0)
+  end,
   config = function()
     vim.g.firenvim_config = {
       globalSettings = {
