d14885c865f4755c8c997d721a47a266ca8b0c68
Luco Bellic <luco.bellic@protonmail.com>
2024-07-30 14:42:48 +0200

    🔧(persistence): update keymaps and autocmds

--- lua/plugins/popup/persistence.lua
+++ lua/plugins/popup/persistence.lua
@@ -3,6 +3,7 @@ local util = require('util.persistence')
 return {
   'lucobellic/persistence.nvim',
   branch = 'personal',
+  event = 'VeryLazy',
   keys = {
     {
       '<leader>qr',
@@ -29,4 +30,25 @@ return {
     options = { 'buffers', 'curdir', 'tabpages', 'winpos', 'folds', 'winsize', 'help', 'globals', 'skiprtp' },
     pre_save = util.pre_save,
   },
+  config = function(_, opts)
+    local persistence = require('persistence')
+    persistence.setup(opts)
+
+    -- Save current directory on exit
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      callback = function() persistence.save() end,
+    })
+
+    -- Load session from persistence
+    vim.api.nvim_create_user_command(
+      'PersistenceLoadSession',
+      function() require('util.persistence').select_session() end,
+      {}
+    )
+    vim.api.nvim_create_user_command(
+      'PersistenceLoadLast',
+      function() require('persistence').load({ last = true }) end,
+      {}
+    )
+  end,
 }
