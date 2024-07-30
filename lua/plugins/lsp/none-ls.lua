3b28f848e7787038312753fa0995dcec6740d981
Luco Bellic <luco.bellic@protonmail.com>
2024-07-30 14:50:00 +0200

    ✨(none-ls): add nix support

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -12,10 +12,8 @@ return {
     table.insert(opts.sources, nls.builtins.formatting.prettierd)
     table.insert(opts.sources, nls.builtins.diagnostics.markdownlint_cli2)
     table.insert(opts.sources, nls.builtins.formatting.mdformat)
-    -- table.insert(opts.sources, nls.builtins.formatting.ruff)
-    -- table.insert(opts.sources, nls.builtins.completion.spell)
-    -- table.insert(opts.sources, nls.builtins.formatting.codespell)
-
+    table.insert(opts.sources, nls.builtins.code_actions.statix)
+    table.insert(opts.sources, nls.builtins.formatting.nixpkgs_fmt)
     -- local cspell = require('cspell')
     -- table.insert(opts.sources, cspell.diagnostics.with({ filetypes = {}, config = cspell_config }))
     -- table.insert(opts.sources, cspell.code_actions.with({ filetypes = {}, config = cspell_config }))
