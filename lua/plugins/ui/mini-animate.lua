4cf9a897c6aebe719e45eaa5214802d9f704d962
Luco Bellic <luco.bellic@protonmail.com>
2024-08-09 15:16:54 +0200

    📱(mini.animate): enabled cursor and smooth scrolling

--- lua/plugins/ui/mini-animate.lua
+++ lua/plugins/ui/mini-animate.lua
@@ -1,16 +1,33 @@
+local latest_timestamp = 0
+local throttle_ms = 50
+local throttle_scroll = function()
+  local cur_time = vim.loop.hrtime()
+  local res = 0.000001 * (cur_time - latest_timestamp) > throttle_ms
+  if res then
+    latest_timestamp = cur_time
+  end
+  return res
+end
+
 return {
   'echasnovski/mini.animate',
-  -- enabled = not vim.g.neovide,
-  enabled = false,
+  enabled = not vim.g.neovide and not vim.g.started_by_firenvim,
   opts = {
-    cursor = {
+    open = {
       enable = false,
+    },
+    close = {
+      enable = false,
+    },
+    cursor = {
+      enable = true,
     },
     scroll = {
       enable = true,
+      subscroll = require('mini.animate').gen_subscroll.equal({ predicate = throttle_scroll }),
     },
     resize = {
-      enabled = false,
-    }
+      enable = false, -- Do not work while holding resize key
+    },
   },
 }
