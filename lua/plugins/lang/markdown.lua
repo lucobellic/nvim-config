ff26a4bf599c90864099d0cee7459912ab001db2
Luco Bellic <luco.bellic@protonmail.com>
2024-09-20 01:54:24 +0200

    👽️(markdown): update repo name to match lazyvim

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -14,7 +14,7 @@ return {
     },
   },
   {
-    'MeanderingProgrammer/markdown.nvim',
+    'MeanderingProgrammer/render-markdown.nvim',
     ft = { 'markdown', 'codecompanion' },
     opts = {
       file_types = { 'markdown', 'codecompanion' },
