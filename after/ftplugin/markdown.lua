f37b597f1ebe5af98034fff559e8835a604e0901
Luco Bellic <luco.bellic@protonmail.com>
2024-09-20 00:45:52 +0200

    🔥(markdown) remove unused after ftplugin

--- after/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
-local buf = vim.api.nvim_get_current_buf()
-local win = vim.api.nvim_get_current_win()
-if require('util.util').is_gp_file(vim.api.nvim_buf_get_name(buf)) then
-  vim.api.nvim_set_option_value('spell', false, { scope = 'local', win = win })
-  vim.api.nvim_set_option_value('number', false, { scope = 'local', win = win })
-  vim.diagnostic.enable(false)
-end
