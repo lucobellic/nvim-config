6e13a33aa7ec4f7afe73ceebee80a69709801490
Luco Bellic <luco.bellic@protonmail.com>
2024-10-06 16:43:48 +0200

    💄(markview): configure options and visualization

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -6,10 +6,55 @@ return {
   {
     'OXY2DEV/markview.nvim',
     enabled = false, -- Too buggy and slow to be usable
+    ft = { 'markdown', 'codecompanion', 'quarto', 'rmd' },
     opts = {
+      states = {
+        { ' ', '+', 'x' },
+      },
+      filetypes = { 'markdown', 'quarto', 'rmd', 'codecompanion' },
       checkboxes = {
         checked = { text = '󱍧', hl = 'DiagnosticOk' },
         unchecked = { text = '󱍫', hl = 'DiagnosticInfo' },
+        custom = {
+          {
+            match_string = '+',
+            text = '󱍬',
+            hl = 'DiagnosticInfo',
+            scope_hl = nil,
+          },
+          {
+            match_string = 'x',
+            text = '󱍮',
+            hl = 'DiagnosticError',
+            scope_hl = nil,
+          },
+          {
+            match_string = '?',
+            text = '󱍥',
+            hl = 'DiagnosticWarn',
+            scope_hl = nil,
+          },
+        },
+      },
+      headings = {
+        shift_width = 0,
+        heading_1 = { sign = '', icons = { '󰉫' } },
+        heading_2 = { sign = '', icons = { '󰉬' } },
+        heading_3 = { sign = '', icons = { '󰉭' } },
+        heading_4 = { sign = '', icons = { '󰉮' } },
+        heading_5 = { sign = '', icons = { '󰉯' } },
+        heading_6 = { sign = '', icons = { '󰉰' } },
+      },
+      code_blocks = { sign = false },
+      list_items = {
+        indent_size = 0,
+        shift_width = 0,
+        marker_dot = {
+          add_padding = false,
+        },
+        marker_parenthesis = {
+          add_padding = false,
+        },
       },
     },
   },
