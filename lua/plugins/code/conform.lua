09cbb0c3b27a5141c44b216c9ce3a7cc063b6e4d
Luco Bellic <luco.bellic@protonmail.com>
2024-10-08 21:55:03 +0200

    ✨(format): enable conform.nvim

--- lua/plugins/code/conform.lua
+++ lua/plugins/code/conform.lua
@@ -1,10 +1,12 @@
 return {
   'stevearc/conform.nvim',
-  enabled = false,
-  opts = {
-    formatters_by_ft = {
-      cpp = { 'clang_format' },
-      markdown = { 'markdownlint-cli2', 'markdown-toc' },
+  keys = {
+    {
+      '<leader>cF',
+      function() require('conform').format({ formatters = { 'injected' }, timeout_ms = 3000 }) end,
+      mode = { 'n', 'v' },
+      desc = 'Format Injected Langs',
     },
   },
+  opts = {},
 }
