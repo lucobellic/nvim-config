c1b9f2f9e5bf449db9b9351053634c8ebe7cc105
Luco Bellic <luco.bellic@protonmail.com>
2025-03-11 10:03:39 +0100

    🔧(mcphub): update servers file name

--- lua/plugins/completion/mcphub.lua
+++ lua/plugins/completion/mcphub.lua
@@ -1,9 +1,10 @@
 return {
   'ravitemer/mcphub.nvim',
+  enabled = vim.fn.executable('npm') == 1,
   dependencies = { 'nvim-lua/plenary.nvim' },
   build = 'npm install -g mcp-hub@latest',
   opts = {
     port = 3000,
-    config = vim.fn.expand('~/.config/nvim/.mcp/mcpservers.json'),
+    config = vim.fn.expand('~/.config/nvim/.mcp/mcp.json'),
   },
 }
