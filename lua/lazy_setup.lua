2e0b47d799fc3af3f4b899db71304fa6690a51b3
Luco Bellic <luco.bellic@protonmail.com>
2025-03-28 22:49:16 +0100

    ✨(lazy): explicitly add lazyvim extras

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -12,6 +12,42 @@ require('lazy').setup({
   spec = {
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
 
+    -- Extras
+    { import = 'lazyvim.plugins.extras.coding.mini-surround' },
+    { import = 'lazyvim.plugins.extras.coding.neogen' },
+    { import = 'lazyvim.plugins.extras.coding.nvim-cmp' },
+    { import = 'lazyvim.plugins.extras.coding.yanky' },
+
+    { import = 'lazyvim.plugins.extras.test.core' },
+    { import = 'lazyvim.plugins.extras.dap.core' },
+    { import = 'lazyvim.plugins.extras.dap.nlua' },
+
+    { import = 'lazyvim.plugins.extras.editor.harpoon2' },
+    { import = 'lazyvim.plugins.extras.editor.snacks_picker' },
+
+    { import = 'lazyvim.plugins.extras.lang.ansible' },
+    { import = 'lazyvim.plugins.extras.lang.clangd' },
+    { import = 'lazyvim.plugins.extras.lang.cmake' },
+    { import = 'lazyvim.plugins.extras.lang.docker' },
+    { import = 'lazyvim.plugins.extras.lang.git' },
+    { import = 'lazyvim.plugins.extras.lang.json' },
+    { import = 'lazyvim.plugins.extras.lang.markdown' },
+    { import = 'lazyvim.plugins.extras.lang.python' },
+    { import = 'lazyvim.plugins.extras.lang.rust' },
+    { import = 'lazyvim.plugins.extras.lang.toml' },
+    { import = 'lazyvim.plugins.extras.lang.typescript' },
+    { import = 'lazyvim.plugins.extras.lang.yaml' },
+
+    { import = 'lazyvim.plugins.extras.lsp.none-ls' },
+    { import = 'lazyvim.plugins.extras.lsp.neoconf' },
+
+    { import = 'lazyvim.plugins.extras.ui.indent-blankline' },
+
+    { import = 'lazyvim.plugins.extras.util.dot' },
+    { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+    { import = 'lazyvim.plugins.extras.util.octo' },
+    { import = 'lazyvim.plugins.extras.vscode' },
+
     { import = 'plugins' },
     { import = 'plugins.code' },
     { import = 'plugins.completion' },
