82ba2c522948f298fccf993675365506dbc9743b
Luco Bellic <luco.bellic@protonmail.com>
2025-04-01 16:05:47 +0200

    ♻️(copilot): merge copilot.lua and .vim together

--- lua/plugins/completion/copilot.lua
+++ lua/plugins/completion/copilot.lua
@@ -34,37 +34,78 @@ local get_keys = function()
 end
 
 return {
-  'zbirenbaum/copilot.lua',
-  enabled = false,
-  keys = get_keys(),
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = false,
-      keymap = {
-        accept = false,
-        next = false,
-        prev = false,
+  {
+    'zbirenbaum/copilot.lua',
+    enabled = vim.g.suggestions == 'copilot',
+    keys = get_keys(),
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = false,
+          next = false,
+          prev = false,
+        },
       },
-    },
-    filetypes = {
-      markdown = true,
-      gitcommit = true,
-      gitrebase = true,
-      hgcommit = true,
-      svn = true,
-    },
-    server_opts_overrides = {
-      settings = {
-        advanced = {
-          inlineSuggestCount = 3,
+      filetypes = {
+        markdown = true,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = true,
+        svn = true,
+      },
+      server_opts_overrides = {
+        settings = {
+          advanced = {
+            inlineSuggestCount = 3,
+          },
         },
       },
     },
+    config = function(_, opts)
+      require('copilot').setup(opts)
+      vim.cmd('Copilot disable')
+    end,
   },
-  config = function(_, opts)
-    require('copilot').setup(opts)
-    vim.cmd('Copilot disable')
-  end,
+  {
+    'github/copilot.vim',
+    enabled = false,
+    event = 'BufEnter',
+    keys = {
+      {
+        '<c-l>',
+        function() vim.fn['copilot#Next']() end,
+        desc = 'Copilot next',
+        mode = 'i',
+      },
+      {
+        '<c-h>',
+        function() vim.fn['copilot#Prev']() end,
+        desc = 'Copilot prev',
+        mode = 'i',
+      },
+      {
+        '<a-l>',
+        '<Plug>(copilot-accept-word)',
+        desc = 'Copilot accept word',
+        mode = 'i',
+      },
+    },
+    init = function()
+      vim.g.copilot_workspace_folders = { '~/.config/nvim' }
+      vim.g.copilot_settings = { selectedCompletionModel = 'gpt-4o-copilot' }
+      vim.g.copilot_integration_id = 'vscode-chat'
+
+      if vim.env.INSIDE_DOCKER then
+        table.insert(vim.g.copilot_workspace_folders, '~/rapidash')
+      end
+    end,
+    config = function()
+      if vim.env.INSIDE_DOCKER then
+        vim.cmd('Copilot disable')
+      end
+    end,
+  },
 }
