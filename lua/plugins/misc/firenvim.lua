d76dc462822f6c70bc9fa9783bd9d4479cf09294
Luco Bellic <luco.bellic@protonmail.com>
2025-04-03 17:58:03 +0200

    💄(firenvim): add minimal size to resize firenvim windows

--- lua/plugins/misc/firenvim.lua
+++ lua/plugins/misc/firenvim.lua
@@ -5,7 +5,7 @@ return {
   lazy = false,
   cond = not not vim.g.started_by_firenvim,
   build = function()
-    require('lazy').load({ plugins = 'firenvim', wait = true })
+    require('lazy').load({ plugins = { 'firenvim' }, wait = true })
     vim.fn['firenvim#install'](0)
   end,
   config = function()
@@ -24,5 +24,22 @@ return {
         },
       },
     }
+
+    -- Resize minimum window height and width on buffer enter
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = '*',
+      callback = function()
+        local win_height = vim.api.nvim_win_get_height(0)
+        local win_width = vim.api.nvim_win_get_width(0)
+        if win_height < 12 then
+          vim.opt.lines = 12
+        end
+
+        if win_width < 80 then
+          vim.opt.columns = 80
+        end
+      end,
+      desc = 'Set minimum window height and width on buffer enter',
+    })
   end,
 }
