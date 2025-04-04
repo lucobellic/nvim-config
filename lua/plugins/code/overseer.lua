e9cb6f40a7d5622bbc056767832e97049c3f90b2
Luco Bellic <luco.bellic@protonmail.com>
2025-04-04 12:48:15 +0200

    🩹(overseer): fix start from terminal command

--- lua/plugins/code/overseer.lua
+++ lua/plugins/code/overseer.lua
@@ -1,24 +1,3 @@
-vim.api.nvim_create_user_command('OverseerRestartLast', function()
-  local overseer = require('overseer')
-  local tasks = overseer.list_tasks({ recent_first = true })
-  if vim.tbl_isempty(tasks) then
-    vim.notify('No tasks found', vim.log.levels.WARN, { title = 'Overseer' })
-  else
-    overseer.run_action(tasks[1], 'restart')
-  end
-end, {})
-
-vim.api.nvim_create_user_command('OverseerFromTerminal', function()
-  -- Use last line of the terminal buffer
-  local cursor_row = vim.api.nvim_win_get_cursor(0)[1]
-  local cmd = vim.api.nvim_buf_get_lines(0, cursor_row - 1, cursor_row, false)[1]
-  cmd = cmd:gsub('^%S+%s*', '')
-  cmd = cmd:gsub('%s*%S+$', '')
-
-  vim.notify('Start: ' .. cmd, vim.log.levels.INFO, { title = 'Overseer' })
-  require('overseer').new_task({ cmd = cmd }):start()
-end, {})
-
 local function open_first_failed_task()
   local overseer = require('overseer')
   local constants = require('overseer.constants')
@@ -100,13 +79,15 @@ return {
     config = function(_, opts)
       local overseer = require('overseer')
       local task_list = require('overseer.task_list')
-      local util = require("overseer.util")
+      local util = require('overseer.util')
 
       -- Override run_in_cwd to prevent fullscreen terminal execution and output flickering
+      ---@diagnostic disable-next-line: duplicate-set-field
       util.run_in_cwd = function(cwd, callback)
         vim.cmd.lcd({ args = { cwd }, mods = { silent = true, noautocmd = true } })
         callback()
       end
+      overseer.setup(opts)
 
       -- Close overseer window when all buffers are closed
       vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
@@ -129,7 +110,26 @@ return {
         end,
       })
 
-      overseer.setup(opts)
+      vim.api.nvim_create_user_command('OverseerRestartLast', function()
+        local overseer = require('overseer')
+        local tasks = overseer.list_tasks({ recent_first = true })
+        if vim.tbl_isempty(tasks) then
+          vim.notify('No tasks found', vim.log.levels.WARN, { title = 'Overseer' })
+        else
+          overseer.run_action(tasks[1], 'restart')
+        end
+      end, {})
+
+      vim.api.nvim_create_user_command('OverseerFromTerminal', function()
+        -- Use last line of the terminal buffer
+        local cursor_row = vim.api.nvim_win_get_cursor(0)[1]
+        local cmd = vim.api.nvim_buf_get_lines(0, cursor_row - 1, cursor_row, false)[1]
+        cmd = cmd:gsub('^%S+%s*', '') -- remove the first word (terminal icon such as ~)
+        cmd = cmd:gsub('%s*%S+%s*$', '') -- remove the last word
+
+        vim.notify('Starting ' .. cmd, vim.log.levels.INFO, { title = 'Overseer' })
+        require('overseer').new_task({ cmd = cmd }):start()
+      end, {})
     end,
   },
 }
