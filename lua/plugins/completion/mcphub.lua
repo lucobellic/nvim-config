fe5dd6cd7b53b8f2a3e3e20bd400ae5b8096714e
Luco Bellic <luco.bellic@protonmail.com>
2025-04-08 16:22:54 +0200

    🔧(mcphub): configure mcphub with codecompanion

--- lua/plugins/completion/mcphub.lua
+++ lua/plugins/completion/mcphub.lua
@@ -6,5 +6,20 @@ return {
   opts = {
     port = 3000,
     config = vim.fn.expand('~/.config/nvim/.mcp/mcp.json'),
+    extensions = {
+      codecompanion = {
+        show_result_in_chat = true,
+        make_vars = true,
+      },
+    },
+    ui = {
+      window = {
+        border = vim.g.winborder,
+      },
+      wo = {
+        winblend = vim.o.winblend,
+        winhl = 'MCPHubMuted:Normal',
+      },
+    },
   },
 }
