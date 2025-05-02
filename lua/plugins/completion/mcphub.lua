4c89e282957adcfb3b91c8a83ce6147673d86087
Luco Bellic <luco.bellic@protonmail.com>
2025-05-02 21:52:42 +0200

    👽️(mcphub): change codecompanion config with latest changes

--- lua/plugins/completion/mcphub.lua
+++ lua/plugins/completion/mcphub.lua
@@ -32,13 +32,13 @@ return {
   {
     'olimorris/codecompanion.nvim',
     opts = {
-      strategies = {
-        chat = {
-          tools = {
-            mcp = {
-              callback = function() return require('mcphub.extensions.codecompanion') end,
-              description = 'Call tools and resources from the MCP Servers',
-            },
+      extensions = {
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
           },
         },
       },
