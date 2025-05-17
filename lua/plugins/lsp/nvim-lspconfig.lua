a433c6494a2535ba84c3ccb780a2040bd0a657ac
Luco Bellic <luco.bellic@protonmail.com>
2025-05-17 10:36:58 +0200

    ♻️(nvim-lspconfig): define lsp keymaps using lazyvim keys

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,5 +1,17 @@
 if vim.g.vscode then
-  require('plugins.lsp.util.keymaps').setup_vscode()
+  local vscode = require('vscode')
+  vim.keymap.set(
+    'n',
+    'gr',
+    function() vscode.action('references-view.findReferences') end,
+    { desc = 'Find All References' }
+  )
+  vim.keymap.set('n', '>D', function() vscode.action('editor.action.marker.next') end, { desc = 'Next Diagnostic' })
+  vim.keymap.set('n', '>E', function() vscode.action('editor.action.marker.next') end, { desc = 'Next Diagnostic' })
+  vim.keymap.set('n', '>W', function() vscode.action('editor.action.marker.next') end, { desc = 'Next Diagnostic' })
+  vim.keymap.set('n', '<D', function() vscode.action('editor.action.marker.prev') end, { desc = 'Prev Diagnostic' })
+  vim.keymap.set('n', '<E', function() vscode.action('editor.action.marker.prev') end, { desc = 'Prev Diagnostic' })
+  vim.keymap.set('n', '<W', function() vscode.action('editor.action.marker.prev') end, { desc = 'Prev Diagnostic' })
 end
 
 return {
@@ -53,7 +65,90 @@ return {
       })
 
       require('lspconfig.ui.windows').default_options.border = vim.g.border.style
-      require('plugins.lsp.util.keymaps').setup()
+      -- require('plugins.lsp.util.keymaps').setup()
+      return opts
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = function(_, opts)
+      local Keys = require('lazyvim.plugins.lsp.keymaps').get()
+      vim.list_extend(Keys, {
+        { '<leader>cc', false, mode = { 'n', 'v' } },
+        { '<leader>cC', false, mode = { 'n', 'v' } },
+        { '<c-k>', false, mode = { 'n', 'v', 'i' } },
+        { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition', has = 'definition' },
+        { 'gr', function() Snacks.picker.lsp_references() end, nowait = true, desc = 'References' },
+        { 'gI', function() Snacks.picker.lsp_implementations() end, desc = 'Goto Implementation' },
+        { 'gy', function() Snacks.picker.lsp_type_definitions() end, desc = 'Goto T[y]pe Definition' },
+        {
+          '<leader>ss',
+          function() Snacks.picker.lsp_symbols({ filter = LazyVim.config.kind_filter }) end,
+          desc = 'LSP Symbols',
+          has = 'documentSymbol',
+        },
+        {
+          '<leader>sS',
+          function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end,
+          desc = 'LSP Workspace Symbols',
+          has = 'workspace/symbols',
+        },
+        { 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>' },
+        { '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' },
+        { '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' },
+        { '<leader>rf', '<cmd>lua vim.lsp.buf.code_action({"refactor"})<CR>' },
+        {
+          '<leader>=',
+          function() require('lazyvim.util.format').format({ force = true }) end,
+          desc = 'Format Document',
+          has = 'documentFormatting',
+        },
+        {
+          '<leader>=',
+          function() require('lazyvim.util.format').format({ force = true }) end,
+          desc = 'Format Range',
+          mode = 'v',
+          has = 'documentRangeFormatting',
+        },
+        { '<F2>', '<cmd>Lspsaga rename<CR>' },
+        { 'gK', '<cmd>Lspsaga hover_doc<CR>' },
+        {
+          '[d',
+          function() require('lspsaga.diagnostic'):goto_prev({ severity = { min = vim.diagnostic.severity.HINT } }) end,
+          repeatable = true,
+          desc = 'Previous Diagnostic',
+        },
+        {
+          ']d',
+          function() require('lspsaga.diagnostic'):goto_next({ severity = { min = vim.diagnostic.severity.HINT } }) end,
+          repeatable = true,
+          desc = 'Next Diagnostic',
+        },
+        {
+          '[w',
+          function() require('lspsaga.diagnostic'):goto_prev({ severity = { min = vim.diagnostic.severity.WARN } }) end,
+          repeatable = true,
+          desc = 'Previous Warning',
+        },
+        {
+          ']w',
+          function() require('lspsaga.diagnostic'):goto_next({ severity = { min = vim.diagnostic.severity.WARN } }) end,
+          repeatable = true,
+          desc = 'Next Warning',
+        },
+        {
+          '[e',
+          function() require('lspsaga.diagnostic'):goto_prev({ severity = vim.diagnostic.severity.ERROR }) end,
+          repeatable = true,
+          desc = 'Previous Error',
+        },
+        {
+          ']e',
+          function() require('lspsaga.diagnostic'):goto_next({ severity = vim.diagnostic.severity.ERROR }) end,
+          repeatable = true,
+          desc = 'Next Error',
+        },
+      })
       return opts
     end,
   },
