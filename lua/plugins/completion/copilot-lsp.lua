48c2f9f89b7e8aa50e8eb2d49684fb170cc13526
Luco Bellic <luco.bellic@protonmail.com>
2025-05-18 14:48:00 +0200

    🔧(copilot-lsp): update configuration and disable

--- lua/plugins/completion/copilot-lsp.lua
+++ lua/plugins/completion/copilot-lsp.lua
@@ -1,14 +1,37 @@
 return {
-  'copilotlsp-nvim/copilot-lsp',
-  enabled = false,
-  lazy = false,
-  keys = {
-    {
-      '<tab>',
-      function() require('copilot-lsp.nes').apply_pending_nes() end,
-      mode = { 'n', 'v' },
-      desc = 'Copilot NES',
+  {
+    'copilotlsp-nvim/copilot-lsp',
+    enabled = false, -- Too slow and do not use copilot lsp handleNextEditRequest
+    lazy = false,
+    dependencies = {
+      {
+        'mason-org/mason.nvim',
+        optional = true,
+        lazy = false,
+        opts = { ensure_installed = { 'copilot-language-server' } },
+      },
     },
+    keys = {
+      {
+        '<S-Tab>',
+        function()
+          nes = require('copilot-lsp.nes')
+          if not nes.walk_cursor_start_edit() then
+            if nes.apply_pending_nes() then
+              nes.walk_cursor_end_edit()
+            end
+          end
+        end,
+        mode = { 'i', 'n', 'v' },
+        desc = 'Copilot NES',
+      },
+    },
+    opts = {},
+    init = function() vim.g.copilot_nes_debounce = 500 end,
+    config = function()
+      vim.lsp.config('copilot_ls', {})
+      vim.lsp.enable('copilot_ls')
+    end,
   },
   opts = {},
   init = function() vim.g.copilot_nes_debounce = 500 end,
