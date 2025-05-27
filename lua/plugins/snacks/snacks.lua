bff24d3cb14ba5ca1efaae887923ef4027a0da2e
Luco Bellic <luco.bellic@protonmail.com>
2025-05-27 11:36:21 +0200

    🔧(snacks): add zen mode keybindings
    - Add <leader>zz for toggling zen mode
    - Add <leader>zm for toggling zoom mode
    - Add <c-z> as alternative zen mode toggle

--- lua/plugins/snacks/snacks.lua
+++ lua/plugins/snacks/snacks.lua
@@ -3,6 +3,9 @@ return {
   keys = {
     { '<leader>..', function() require('snacks').scratch() end, desc = 'Scratch Toggle Buffer' },
     { '<leader>f.', function() require('snacks').scratch.select() end, desc = 'Scratch Select Buffer' },
+    { '<leader>zz', function() require('snacks.zen').zen() end, desc = 'Toggle Zen Mode' },
+    { '<leader>zm', function() require('snacks.zen').zoom() end, desc = 'Toggle Zoom Mode' },
+    { '<c-z>', function() require('snacks.zen').zen() end, desc = 'Toggle Zen Mode' },
     { '<leader>fe', false },
   },
   opts = function()
