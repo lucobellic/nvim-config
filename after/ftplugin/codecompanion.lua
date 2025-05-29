465454c05fa02535e5eb8e336b53be9856035527
Luco Bellic <luco.bellic@protonmail.com>
2025-05-29 07:35:37 +0200

    ✨(codecompanion): add <c-cr> keymap to send chat and exit insert mode
    - Add convenient keymap for submitting chat without leaving insert mode
    - Improves workflow efficiency for CodeCompanion interactions

--- after/ftplugin/codecompanion.lua
+++ after/ftplugin/codecompanion.lua
@@ -1,1 +1,7 @@
 vim.keymap.set('n', 'gf', function() require('util.util').open_file() end, { buffer = 0 })
+vim.keymap.set('i', '<c-cr>', function()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', false)
+  --- @type CodeCompanion.Chat
+  local chat = require('codecompanion').buf_get_chat(0)
+  chat:submit()
+end, { desc = 'Send CodeCompanion Chat and Exit Insert Mode' })
