2edcc52ef7777bc1cacf3005e578f16e5843bd76
Luco Bellic <luco.bellic@protonmail.com>
2025-06-04 13:48:00 +0200

    🔧(keymaps): remove conflicting <c-/> keymap
    - Delete <c-/> keymap to prevent conflicts with other plugins
    - Maintains existing change operator tracking functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,6 @@ vim.keymap.del('n', '<leader>n')
 vim.keymap.del('n', '<leader>e')
 vim.keymap.del('n', '<leader>.')
 vim.keymap.del('n', '<leader>ud')
+vim.keymap.del('n', '<c-/>')
 
 vim.keymap.set({ 'n', 'v' }, 'c', '<cmd>lua vim.g.change = true<cr>c', { noremap = true, desc = 'Change' })
