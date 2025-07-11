8f6f0cbdb2b2caaeb1b865d9123952d5db2f2c86
Luco Bellic <luco.bellic@protonmail.com>
2025-07-11 21:42:33 +0200

    ✨(vscode): add vim-shareedit VSCode integration
    - Add VSCode-specific keymaps for shareedit connection/disconnection
    - Map <leader>ue to shareedit.connect action
    - Map <leader>uE to shareedit.disconnect action
    - Enable collaborative editing functionality in VSCode environment

--- lua/plugins/misc/vim-shareedit.lua
+++ lua/plugins/misc/vim-shareedit.lua
@@ -1,3 +1,9 @@
+if vim.g.vscode then
+  local vscode = require('vscode')
+  vim.keymap.set('n', '<leader>ue', function() vscode.action('shareedit.connect') end, { desc = 'Share Edit Start' })
+  vim.keymap.set('n', '<leader>uE', function() vscode.action('shareedit.disconnect') end, { desc = 'Share Edit End' })
+end
+
 return {
   'kbwo/vim-shareedit',
   enabled = not vim.g.vscode,
