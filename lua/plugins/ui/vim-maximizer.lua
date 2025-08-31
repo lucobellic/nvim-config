b3aca50ebee735102b2ebccb96e0301d6b77e429
Luco Bellic <luco.bellic@protonmail.com>
2025-08-31 21:10:30 +0200

    ➕(vim-maximizer): add plugin for maximizing and restoring windows

---
+++ lua/plugins/ui/vim-maximizer.lua
@@ -0,0 +1,16 @@
+return {
+  'szw/vim-maximizer',
+  keys = {
+    {
+      '<leader>wm',
+      function() vim.cmd('MaximizerToggle!') end,
+      repeatable = true,
+      desc = 'Toggle Maximizer',
+    },
+  },
+  init = function()
+    vim.g.maximizer_set_default_mapping = 0
+    vim.g.maximizer_restore_on_winleave = 1
+  end,
+  config = function() end,
+}
