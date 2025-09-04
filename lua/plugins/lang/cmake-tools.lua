2c643f385a4092604505244b5e35b5eef5f12772
Luco Bellic <luco.bellic@protonmail.com>
2025-09-04 21:26:55 +0200

    ✨(cmake-tools): enhance CMake development support
    - Add new CMake tools configuration with extended functionality
    - Improve CMake project build and debug capabilities
    - Enhance C/C++ development workflow with better CMake integration

--- lua/plugins/lang/cmake-tools.lua
+++ lua/plugins/lang/cmake-tools.lua
@@ -1,3 +1,23 @@
+--- Find the first CMakeLists.txt file in the given path or its parent directories
+---@param path? string The starting path to search from or current file's directory if nil
+local function find_closest_cmake_lists(path)
+  local path = path or vim.fn.expand('%:p:h')
+  return vim.fs.find({ 'CMakeLists.txt' }, { path = path, upward = true, limit = 1 })[1]
+end
+
+local function jump_to_cmake_lists()
+  local cmake_file = find_closest_cmake_lists()
+  if cmake_file then
+    vim.cmd('edit ' .. cmake_file)
+  else
+    vim.notify(
+      'No CMakeLists.txt found in the current directory or its parents.',
+      vim.log.levels.INFO,
+      { title = 'CMake' }
+    )
+  end
+end
+
 return {
   'Civitasv/cmake-tools.nvim',
   init = function() end,
@@ -36,6 +56,9 @@ return {
     'CMakeSelectBuildTarget',
     'CMakeSelectLaunchTarget',
     'CMakeSelectConfigurePreset',
+  },
+  keys = {
+    { '<leader>oc', jump_to_cmake_lists, desc = 'Jump to CMakeLists.txt' },
   },
   opts = {
     cmake_executor = {
