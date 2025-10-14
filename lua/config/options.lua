7f01c1fb83ff2890cfe26fdfde6bf75a1a8d766a
Luco Bellic <luco.bellic@protonmail.com>
2025-10-14 23:22:57 +0200

    🔧(options): change maplocalleader from backslash to underscore

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.maplocalleader = '_'
 vim.g.autoformat = false
 vim.g.markdown_folding = true
 
