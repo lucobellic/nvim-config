2044d8aad1e44c6610a4d4e2ff41616162e5b5be
Luco Bellic <luco.bellic@protonmail.com>
2025-10-14 23:23:46 +0200

    ♻️(ezi): refactor treesitter parser setup with autocmd

--- lua/plugins/code/ezi.lua
+++ lua/plugins/code/ezi.lua
@@ -1,20 +1,17 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  enabled = false,
-  cond = false,
   opts = function(_, opts)
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    --- @diagnostic disable-next-line: inject-field
-    parser_config.ezi = {
-      install_info = {
-        url = 'git@gitlab.easymile.com:blaiselcq/tree-sitter-ezi', -- local path or git repo
-        files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-        branch = 'master', -- default branch in case of git repo if different from master
-        generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-      },
-      maintainers = { '@blaiselcq' },
-    }
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'TSUpdate',
+      callback = function()
+        require('nvim-treesitter.parsers').ezi = {
+          install_info = {
+            path = '~/Development/tools/tree-sitter-ezi',
+          },
+          maintainers = { '@blaiselcq' },
+        }
+      end,
+    })
     return opts
   end,
 }
