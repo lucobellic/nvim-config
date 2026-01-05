1b8705cff9b3dcdf7bfbd1f15f9eb8b7ad85013b
Luco Bellic <luco.bellic@protonmail.com>
2026-01-05 22:27:12 +0100

    🔧 (kitty-scrollback): update conditions and disable paste window

--- lua/plugins/misc/kitty-scrollback.lua
+++ lua/plugins/misc/kitty-scrollback.lua
@@ -1,6 +1,11 @@
 return {
   'mikesmithgh/kitty-scrollback.nvim',
+  cond = vim.env.TERM and vim.env.TERM:find('kitty') and vim.env.KITTY_SCROLLBACK_NVIM == 'true',
   cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
   event = { 'User KittyScrollbackLaunch' },
-  config = function() require('kitty-scrollback').setup() end,
+  opts = {
+    {
+      paste_window = { yank_register_enabled = false },
+    },
+  },
 }
