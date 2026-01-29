ae8adbba42f89185c8b9c0ad07292e1fea87b894
Luco Bellic <luco.bellic@protonmail.com>
2026-01-29 22:47:22 +0100

    🔧 (ezi): update installation setup using git url

--- lua/plugins/code/ezi.lua
+++ lua/plugins/code/ezi.lua
@@ -1,3 +1,11 @@
+local function git_latest(url)
+  local path = vim.fn.stdpath('data') .. '/tree-sitter-ezi'
+  local is_installed = vim.fn.isdirectory(path) == 1
+  local command = is_installed and { 'git', '-C', path, 'pull' } or { 'git', 'clone', url, path }
+  vim.fn.system(command)
+  return path
+end
+
 return {
   'nvim-treesitter/nvim-treesitter',
   opts = function(_, opts)
@@ -6,7 +14,8 @@ return {
       callback = function()
         require('nvim-treesitter.parsers').ezi = {
           install_info = {
-            path = '~/Development/tools/tree-sitter-ezi',
+            path = git_latest('git@gitlab.easymile.com:blaiselcq/tree-sitter-ezi'),
+            queries = 'queries',
           },
           maintainers = { '@blaiselcq' },
         }
