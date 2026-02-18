6ae36a2c5caa24fc2e7f059adec9c533ae905bbc
Luco Bellic <luco.bellic@protonmail.com>
2026-02-18 21:20:03 +0100

    🔧 (codecompanion): relax tool approval settings

--- lua/plugins/codecompanion/tools.lua
+++ lua/plugins/codecompanion/tools.lua
@@ -13,6 +13,40 @@ return {
               require_confirmation_after = false,
             },
           },
+          ['cmd_runner'] = {
+            opts = {
+              allowed_in_yolo_mode = true,
+              require_approval_before = false,
+              require_cmd_approval = false,
+            },
+          },
+          ['create_file'] = {
+            opts = {
+              require_approval_before = false,
+              require_cmd_approval = false,
+            },
+          },
+          ['delete_file'] = {
+            opts = {
+              allowed_in_yolo_mode = true,
+              require_approval_before = false,
+              require_cmd_approval = false,
+            },
+          },
+          ['file_search'] = { opts = { require_cmd_approval = false } },
+          ['grep_search'] = {
+            opts = {
+              require_approval_before = false,
+              require_cmd_approval = false,
+            },
+          },
+          ['memory'] = { opts = { require_approval_before = false } },
+          ['read_file'] = {
+            opts = {
+              require_approval_before = false,
+              require_cmd_approval = false,
+            },
+          },
           opts = {
             default_tools = { 'code_execution', 'full_stack_dev', 'memory' },
             require_approval_before = false,
