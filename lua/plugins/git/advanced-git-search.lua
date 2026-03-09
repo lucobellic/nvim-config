3f05b5ca45c2a87d0f176dcb8ede84de1df1f5ec
Luco Bellic <luco.bellic@protonmail.com>
2026-03-09 22:10:02 +0100

    ➖ (advanced-git-search): remove plugin

--- lua/plugins/git/advanced-git-search.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'aaronhallaert/advanced-git-search.nvim',
-  cmd = { 'AdvancedGitSearch' },
-  opts = {
-    diff_plugin = 'diffview',
-  },
-  config = function(_, opts) require('advanced_git_search.snacks').setup(opts) end,
-  dev = true,
-}
