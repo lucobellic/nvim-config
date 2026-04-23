244fb3cd4819ff1d966e80f677df1eb143394893
Luco Bellic <luco.bellic@protonmail.com>
2026-04-23 17:39:01 +0200

    🔧 (copilot): enable copilot for codecompanion buffer

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,6 +8,16 @@ if vim.fn.has('nvim-0.12') == 1 then
   if vim.g.suggestions == 'copilot' then
     vim.lsp.enable('copilot')
     vim.lsp.inline_completion.enable()
+
+    -- Enable copilot for codecompanion buffers
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'codecompanion' },
+      callback = function(args)
+        if vim.g.suggestions then
+          vim.lsp.start(vim.lsp.config['copilot'], { bufnr = args.buf })
+        end
+      end,
+    })
   end
 end
 
