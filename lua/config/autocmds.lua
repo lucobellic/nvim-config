a753a2c7e38f73d371f717ad6abf5b065cb093a7
Luco Bellic <luco.bellic@protonmail.com>
2026-05-06 16:30:09 +0200

    🔧 (autocmds): Remove automatic spell and wrap autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.api.nvim_clear_autocmds({ group = 'lazyvim_wrap_spell' })
+
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   pattern = { 'cpp' },
   command = 'setlocal commentstring=//\\ %s',
