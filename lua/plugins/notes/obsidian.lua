e05060e67ca0afde0443693af04cacbd46a4becc
Luco Bellic <luco.bellic@protonmail.com>
2026-05-12 15:12:20 +0200

    🔧 (obsidian): configure blink completion for obsidian

--- lua/plugins/notes/obsidian.lua
+++ lua/plugins/notes/obsidian.lua
@@ -28,6 +28,22 @@ local function get_workspaces()
   return workspaces
 end
 
+---@param opts obsidian.config
+local function configure_blink_source(opts)
+  local blink_ok, blink_config = pcall(require, 'blink.cmp.config')
+  if blink_ok then
+    local obsidian_providers = { obsidian = 10, obsidian_tags = 10 }
+    if opts.completion and opts.completion.create_new ~= false then
+      obsidian_providers.obsidian_new = 10
+    end
+    for id, offset in pairs(obsidian_providers) do
+      if blink_config.sources.providers[id] then
+        blink_config.sources.providers[id].score_offset = offset
+      end
+    end
+  end
+end
+
 return {
   {
     'folke/which-key.nvim',
@@ -41,7 +57,7 @@ return {
   },
   {
     'obsidian-nvim/obsidian.nvim',
-    version = '3.15.5',
+    version = '3.16.3',
     cond = vim.fn.isdirectory(vim.fn.expand('~/vaults')) == 1,
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -57,16 +73,21 @@ return {
       { '<leader>nn', '<cmd>ObsidianTask<CR>', desc = 'Obsidian Task' },
       { '<leader>nw', '<cmd>Obsidian workspace<CR>', desc = 'Obsidian Workspace' },
     },
+    ft = { 'markdown' },
     --- @type obsidian.config
     opts = {
       legacy_commands = false,
       workspaces = get_workspaces(),
       notes_subdir = 'notes',
+      templates = { enabled = false },
+      frontmatter = { enabled = false },
       daily_notes = {
         folder = 'dailies',
       },
       completion = {
         nvim_cmp = false, -- if using nvim-cmp, otherwise set to false
+        blink = true,
+        min_chars = 0,
       },
       note_id_func = function(title) return title end,
       ui = {
@@ -77,6 +98,7 @@ return {
     config = function(_, opts)
       require('obsidian').setup(opts)
 
+      configure_blink_source(opts)
       --- Create a new note with the name of the current task
       local function toggle_current_task()
         -- Get name of the current branch
