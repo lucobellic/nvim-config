40be75f0c4f9b81c51706f3ce125a135c844a0d8
Luco Bellic <luco.bellic@protonmail.com>
2026-05-24 16:24:54 +0200

    🔧 (agents): update keymaps for next, previous agent

--- after/ftplugin/cursor-agent.lua
+++ after/ftplugin/cursor-agent.lua
@@ -1,4 +1,6 @@
 vim.keymap.set({ 'n', 't', 'i' }, '<A-l>', '<cmd>CursorNext<cr>', { buffer = true })
 vim.keymap.set({ 'n', 't', 'i' }, '<A-h>', '<cmd>CursorPrev<cr>', { buffer = true })
+vim.keymap.set({ 'n' }, '<S-l>', '<cmd>CursorNext<cr>', { buffer = true })
+vim.keymap.set({ 'n' }, '<S-h>', '<cmd>CursorPrev<cr>', { buffer = true })
 vim.keymap.set({ 'n', 't', 'i' }, '<C-t>', '<cmd>CursorNew<cr>', { buffer = true })
 vim.keymap.set({ 'n', 't', 'i' }, '<C-q>', '<cmd>CursorClose<cr>', { buffer = true })
--- after/ftplugin/opencode.lua
+++ after/ftplugin/opencode.lua
@@ -55,6 +55,8 @@ end
 
 vim.keymap.set({ 'n', 't', 'i' }, '<A-l>', '<cmd>OpenCodeNext<cr>', { buffer = true })
 vim.keymap.set({ 'n', 't', 'i' }, '<A-h>', '<cmd>OpenCodePrev<cr>', { buffer = true })
+vim.keymap.set({ 'n' }, '<S-l>', '<cmd>OpenCodeNext<cr>', { buffer = true })
+vim.keymap.set({ 'n' }, '<S-h>', '<cmd>OpenCodePrev<cr>', { buffer = true })
 vim.keymap.set({ 'n', 't', 'i' }, '<C-q>', '<cmd>OpenCodeClose<cr>', { buffer = true })
 vim.keymap.set({ 'n' }, '<C-t>', '<cmd>OpenCodeNew<cr>', { buffer = true })
 vim.keymap.set({ 'n' }, '<C-x>', function()
