5de9079b6a4acbecfea48ade9c920538065f625d
Luco Bellic <luco.bellic@protonmail.com>
2026-05-24 16:25:33 +0200

    🔧 (vtsls): update configuration to support yarn sdks

--- after/lsp/vtsls.lua
+++ after/lsp/vtsls.lua
@@ -3,13 +3,8 @@ return {
     vtsls = {
       autoUseWorkspaceTsdk = true,
     },
-    typescript = {},
+    typescript = {
+      tsdk = './.yarn/sdks/typescript/lib',
+    },
   },
-  before_init = function(_, config)
-    local pnp = vim.fs.find({ '.pnp.cjs' }, { upward = true })[1]
-    if pnp then
-      config.settings.typescript.tsdk = vim.fs.dirname(pnp) .. '/.yarn/sdks/typescript/lib'
-    end
-  end,
 }
-
