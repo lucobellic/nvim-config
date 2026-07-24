9826419e8e472bb0a66039f7164ec46e8dfa8617
Luco Bellic <luco.bellic@protonmail.com>
2026-07-24 11:10:04 +0200

    🩹 (copy): fix OSC52 copy closing terminal

--- lua/config/docker_clipboard.lua
+++ lua/config/docker_clipboard.lua
@@ -1,15 +1,32 @@
 -- Enable OSC 52 inside docker
 if vim.env.INSIDE_DOCKER then
+  local cache = {
+    ['+'] = { {}, 'v' },
+    ['*'] = { {}, 'v' },
+  }
+
+  local function copy(reg)
+    local osc52_copy = require('vim.ui.clipboard.osc52').copy(reg)
+
+    return function(lines, regtype)
+      cache[reg] = { vim.deepcopy(lines), regtype }
+      osc52_copy(lines)
+    end
+  end
+
+  local function paste(reg)
+    return function() return vim.deepcopy(cache[reg]) end
+  end
+
   vim.g.clipboard = {
-    name = 'OSC 52',
+    name = 'OSC 52 (copy only)',
     copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      ['+'] = copy('+'),
+      ['*'] = copy('*'),
     },
     paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      ['+'] = paste('+'),
+      ['*'] = paste('*'),
     },
-    cache_enabled = true,
   }
 end
