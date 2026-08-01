e895c286db4f5345646cb51a73514837cfd5f120
Luco Bellic <luco.bellic@protonmail.com>
2026-08-01 17:37:21 +0200

    🔧 (neotest): disable neotest python

--- lua/plugins/code/neotest.lua
+++ lua/plugins/code/neotest.lua
@@ -3,17 +3,17 @@ return {
   dependencies = {
     'nvim-neotest/neotest-plenary',
     'nvim-neotest/neotest-vim-test',
-    'nvim-neotest/neotest-python',
+    { 'nvim-neotest/neotest-python', cond = false },
   },
   cmd = { 'Neotest' },
   opts = function(_, opts)
     -- Configure neotest-python
     opts.adapters = opts.adapters or {}
-    opts.adapters['neotest-python'] = vim.tbl_deep_extend('force', opts.adapters['neotest-python'] or {}, {
-      runner = 'pytest',
-      python = '/usr/bin/python',
-      pytest_discover_instances = true, -- experimental
-    })
+    -- opts.adapters['neotest-python'] = vim.tbl_deep_extend('force', opts.adapters['neotest-python'] or {}, {
+    --   runner = 'pytest',
+    --   python = '/usr/bin/python',
+    --   pytest_discover_instances = false, -- experimental
+    -- })
 
     opts = vim.tbl_deep_extend('force', opts, {
       status = {
